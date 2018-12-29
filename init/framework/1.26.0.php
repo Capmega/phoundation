0c35577ecdb95854221937e58e8c6e478bf91ff1
Sven Oostenbrink <so.oostenbrink@gmail.com>
2018-12-28 23:30:05 -0600

    FV1.26.0 Added doc library tables

---
+++ init/framework/1.26.0.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ * Add tables for doc generator library
+ */
+sql_query('DROP TABLE IF EXISTS `doc_generated`');
+sql_query('DROP TABLE IF EXISTS `doc_values`');
+sql_query('DROP TABLE IF EXISTS `doc_pages`');
+sql_query('DROP TABLE IF EXISTS `doc_projects`');
+
+
+
+/*
+ *
+ */
+sql_query('CREATE TABLE `doc_projects` (`id`        INT(11)     NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                        `createdon` TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                        `createdby` INT(11)         NULL DEFAULT NULL,
+                                        `meta_id`   INT(11)     NOT NULL,
+                                        `status`    VARCHAR(16)     NULL DEFAULT NULL,
+                                        `name`      VARCHAR(32) NOT NULL,
+                                        `seoname`   VARCHAR(32) NOT NULL,
+                                        `language`  VARCHAR(2)  NOT NULL,
+
+                                               KEY `createdon` (`createdon`),
+                                               KEY `createdby` (`createdby`),
+                                               KEY `meta_id`   (`meta_id`),
+                                               KEY `status`    (`status`),
+                                               KEY `name`      (`name`),
+                                               KEY `language`  (`language`),
+                                        UNIQUE KEY `seoname`   (`seoname`),
+
+                                        CONSTRAINT `fk_doc_projects_createdby` FOREIGN KEY (`createdby`) REFERENCES `users` (`id`) ON DELETE RESTRICT,
+                                        CONSTRAINT `fk_doc_projects_meta_id`   FOREIGN KEY (`meta_id`)   REFERENCES `meta`  (`id`) ON DELETE RESTRICT
+
+                                       ) ENGINE=InnoDB AUTO_INCREMENT='.$_CONFIG['db']['core']['autoincrement'].' DEFAULT CHARSET="'.$_CONFIG['db']['core']['charset'].'" COLLATE="'.$_CONFIG['db']['core']['collate'].'";');
+
+
+
+/*
+ *
+ */
+sql_query('CREATE TABLE `doc_pages` (`id`          INT(11)     NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                     `createdon`   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                     `createdby`   INT(11)         NULL DEFAULT NULL,
+                                     `meta_id`     INT(11)     NOT NULL,
+                                     `status`      VARCHAR(16)     NULL DEFAULT NULL,
+                                     `projects_id` INT(11)         NULL,
+                                     `parents_id`  INT(11)         NULL,
+                                     `name`        VARCHAR(32) NOT NULL,
+                                     `seoname`     VARCHAR(32) NOT NULL,
+                                     `package`     VARCHAR(32)     NULL,
+                                     `type`        ENUM("function", "class", "library", "chapter", "page", "webpage", "script") NOT NULL,
+
+                                            KEY `createdon`  (`createdon`),
+                                            KEY `createdby`  (`createdby`),
+                                            KEY `meta_id`    (`meta_id`),
+                                            KEY `status`     (`status`),
+                                            KEY `parents_id` (`parents_id`),
+                                            KEY `name`       (`name`),
+                                            KEY `package`    (`package`),
+                                     UNIQUE KEY `seoname`    (`seoname`),
+
+                                     CONSTRAINT `fk_doc_pages_createdby`  FOREIGN KEY (`createdby`)  REFERENCES `users`     (`id`) ON DELETE RESTRICT,
+                                     CONSTRAINT `fk_doc_pages_meta_id`    FOREIGN KEY (`meta_id`)    REFERENCES `meta`      (`id`) ON DELETE RESTRICT,
+                                     CONSTRAINT `fk_doc_pages_parents_id` FOREIGN KEY (`parents_id`) REFERENCES `doc_pages` (`id`) ON DELETE RESTRICT
+
+                                    ) ENGINE=InnoDB AUTO_INCREMENT='.$_CONFIG['db']['core']['autoincrement'].' DEFAULT CHARSET="'.$_CONFIG['db']['core']['charset'].'" COLLATE="'.$_CONFIG['db']['core']['collate'].'";');
+
+
+
+/*
+ *
+ */
+sql_query('CREATE TABLE `doc_values` (`id`        INT(11)     NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                      `createdon` TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                      `createdby` INT(11)         NULL DEFAULT NULL,
+                                      `meta_id`   INT(11)     NOT NULL,
+                                      `status`    VARCHAR(16)     NULL DEFAULT NULL,
+                                      `pages_id`  INT(11)     NOT NULL,
+                                      `key`       ENUM("header", "paragraph", "author", "copyright", "license", "see", "table", "note", "version", "example", "params", "param", "return", "exception") NOT NULL,
+                                      `values`    TEXT            NULL DEFAULT NULL,
+
+                                             KEY `createdon` (`createdon`),
+                                             KEY `createdby` (`createdby`),
+                                             KEY `meta_id`   (`meta_id`),
+                                             KEY `status`    (`status`),
+                                             KEY `pages_id`  (`pages_id`),
+                                             KEY `key`       (`key`),
+
+                                      CONSTRAINT `fk_doc_values_createdby` FOREIGN KEY (`createdby`) REFERENCES `users`     (`id`) ON DELETE RESTRICT,
+                                      CONSTRAINT `fk_doc_values_meta_id`   FOREIGN KEY (`meta_id`)   REFERENCES `meta`      (`id`) ON DELETE RESTRICT,
+                                      CONSTRAINT `fk_doc_values_pages_id`  FOREIGN KEY (`pages_id`)  REFERENCES `doc_pages` (`id`) ON DELETE CASCADE
+
+                                     ) ENGINE=InnoDB AUTO_INCREMENT='.$_CONFIG['db']['core']['autoincrement'].' DEFAULT CHARSET="'.$_CONFIG['db']['core']['charset'].'" COLLATE="'.$_CONFIG['db']['core']['collate'].'";');
+?>
--- www/en/libs/startup.php
+++ www/en/libs/startup.php
@@ -62,7 +62,7 @@
 /*
  * Framework version
  */
-define('FRAMEWORKCODEVERSION', '1.25.0');
+define('FRAMEWORKCODEVERSION', '1.26.0');
 define('PHP_MINIMUM_VERSION' , '5.5.9');
 
 
