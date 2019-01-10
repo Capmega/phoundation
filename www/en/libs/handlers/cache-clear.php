70e498052fe11ab11f220b7a046fea0d75500bbd
Sven Olaf Oostenbrink <sven@capmega.com>
2019-01-10 12:36:36 -0600

    Fixed cache library
    Fixed cache_clear(): Now deletes correct bundler files

--- www/en/libs/handlers/cache-clear.php
+++ www/en/libs/handlers/cache-clear.php
@@ -85,12 +85,12 @@ try{
  * Clear bundler caches
  */
 try{
-    file_delete(ROOT.'www/en/pub/js/bundle/');
-    file_delete(ROOT.'www/en/pub/css/bundle/');
-    file_delete(ROOT.'www/en/admin/pub/js/bundle/');
-    file_delete(ROOT.'www/en/admin/pub/css/bundle/');
+    file_delete(ROOT.'www/en/pub/js/bundle-*');
+    file_delete(ROOT.'www/en/pub/css/bundle-*');
+    file_delete(ROOT.'www/en/admin/pub/js/bundle-*');
+    file_delete(ROOT.'www/en/admin/pub/css/bundle-*');
 
-    log_console(tr('Cleared bundler caches from paths ":path"', array(':path' => 'ROOT/pub/js/bundle,ROOT/pub/css/bundle')));
+    log_console(tr('Cleared bundler caches from paths ":path"', array(':path' => 'ROOT/pub/js/bundle-*,ROOT/pub/css/bundle-*')));
 
 }catch(Exception $e){
     notify('cache-clear/failed', $e);
