e6dcecadecf0fa0abc7612a465d23edc3b95faf0
Sven Olaf Oostenbrink <sven@capmega.com>
2019-01-14 19:35:53 -0600

    Added general exception catch over script_exec()

--- www/en/libs/handlers/startup-script-exec.php
+++ www/en/libs/handlers/startup-script-exec.php
@@ -1,28 +1,33 @@
 <?php
-/*
- * Handler code for script_exec() function
- */
-$script = ROOT.'scripts/'.$script;
+try{
+    /*
+     * Handler code for script_exec() function
+     */
+    $script_file = ROOT.'scripts/'.$script;
 
-if(!file_exists($script)){
-    throw new bException(tr('script_exec(): Specified script ":script" does not exist', array(':script' => $script)), 'not-exist');
-}
-
-if($arguments){
-    if(!is_array($arguments)){
-        throw new bException(tr('script_exec(): Invalid arguments ":arguments" specified, must be an array or null', array(':arguments' => $arguments)), 'invalid');
+    if(!file_exists($script_file)){
+        throw new bException(tr('script_exec(): Specified script ":script" does not exist', array(':script' => $script)), 'not-exist');
     }
 
-    foreach($arguments as &$argument){
-        $argument = escapeshellarg($argument);
+    if($arguments){
+        if(!is_array($arguments)){
+            throw new bException(tr('script_exec(): Invalid arguments ":arguments" specified, must be an array or null', array(':arguments' => $arguments)), 'invalid');
+        }
+
+        foreach($arguments as &$argument){
+            $argument = escapeshellarg($argument);
+        }
+
+        unset($argument);
+
+        $arguments[] = '-E';
+        $arguments[] = ENVIRONMENT;
+        $arguments   = implode(' ', $arguments);
     }
 
-    unset($argument);
+    return safe_exec(escapeshellcmd($script_file).' '.$arguments, $ok_exitcodes);
 
-    $arguments[] = '-E';
-    $arguments[] = ENVIRONMENT;
-    $arguments   = implode(' ', $arguments);
+}catch(Exception $e){
+    throw new bException(tr('script_exec(): Failed to execute script ":script"', array(':script' => $script)), $e);
 }
-
-return safe_exec(escapeshellcmd($script).' '.$arguments, $ok_exitcodes);
 ?>
