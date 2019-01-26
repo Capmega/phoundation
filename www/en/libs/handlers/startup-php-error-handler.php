2e8db26247ee6b8d8e7194319dbf62100b485a82
Sven Olaf Oostenbrink <sven@capmega.com>
2019-01-25 20:37:23 -0600

    Improved startup library
    Simplified PHP exception log output

--- www/en/libs/handlers/startup-php-error-handler.php
+++ www/en/libs/handlers/startup-php-error-handler.php
@@ -5,12 +5,13 @@ if(!$core->register['ready']){
     throw new bException('Pre core-ready PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"', $errno);
 }
 
-$session   = "\n\n\n<br><br>SESSION DATA<br><br>\n\n\n".htmlentities(print_r(isset_get($_SESSION), true));
-$server    = "\n\n\n<br><br>SERVER DATA<br><br>\n\n\n".htmlentities(print_r(isset_get($_SERVER), true));
-$trace     = "\n\nFUNCTION TRACE\n".htmlentities(print_r(debug_backtrace(), true));
+//$session   = "\n\n\n<br><br>SESSION DATA<br><br>\n\n\n".htmlentities(print_r(isset_get($_SESSION), true));
+//$server    = "\n\n\n<br><br>SERVER DATA<br><br>\n\n\n".htmlentities(print_r(isset_get($_SERVER), true));
+$trace     = "\n\nFUNCTION TRACE\n".htmlentities(print_r(debug_trace(), true));
 
 notify(array('title'       => 'php-error',
-             'description' => '<pre> PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"'.$server.$session.$trace.'</pre>',
+//             'description' => '<pre> PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"'.$server.$session.$trace.'</pre>',
+             'description' => '<pre> PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"'.$trace.'</pre>',
              'class'       => 'exception'));
 
 if(PLATFORM_HTTP){
