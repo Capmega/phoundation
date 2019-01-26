f913485fc594db2e6fd602c98caf0bb407ca23ca
Sven Olaf Oostenbrink <sven@capmega.com>
2019-01-25 20:13:42 -0600

    Fixed SSO default configuration

--- config/base/sso.php
+++ config/base/sso.php
@@ -1,4 +1,12 @@
 <?php
 // SSO configuration
-$_CONFIG['sso']['cache_config'] = 86400;
+$_CONFIG['sso']                                                                 = array('cache_config' => 86400,
+                                                                                        'facebook'     => false,
+                                                                                        'google'       => false,
+                                                                                        'linkedin'     => false,
+                                                                                        'microsoft'    => false,
+                                                                                        'paypal'       => false,
+                                                                                        'reddit'       => false,
+                                                                                        'twitter'      => false,
+                                                                                        'yandex'       => false);
 ?>
