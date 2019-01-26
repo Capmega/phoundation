9579d074c0b394891d2d8f4a0b9bcf54524b3095
Sven Olaf Oostenbrink <sven@capmega.com>
2019-01-25 20:49:17 -0600

    Added new library btrfs
    This library will be a front-end library for the btrfs utilities to manage linux BTRFS filesystems

---
+++ www/en/libs/btrfs.php
@@ -0,0 +1,162 @@
+<?php
+/*
+ * BTRFS library
+ *
+ * This is a front-end library to interact with the Linux Butter File System (short BTRFS) management interface
+ *
+ * @author Sven Oostenbrink <support@capmega.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright 2019 Capmega <license@capmega.com>
+ * @category Function reference
+ * @package btrfs
+ */
+
+
+
+/*
+ * Initialize the library, automatically executed by libs_load()
+ *
+ * NOTE: This function is executed automatically by the load_libs() function and does not need to be called manually
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package btrfs
+ *
+ * @return void
+ */
+function btrfs_library_init(){
+    try{
+        ensure_installed(array('name'      => 'btrfs',
+                               'project'   => 'btrfs',
+                               'callback'  => 'btrfs_install',
+                               'checks'    => array('/bin/btrfs')));
+
+    }catch(Exception $e){
+        throw new bException('btrfs_library_init(): Failed', $e);
+    }
+}
+
+
+
+/*
+ * Install the external btrfs  tools
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package btrfs
+ * @note This function typically gets executed automatically bu btrfs_library_init() through the ensure_installed() call, and does not need to be run manually
+ *
+ * @param params $params
+ * @return
+ */
+function btrfs_install($params){
+    try{
+        $params['methods'] = array('apt-get' => array('commands'  => 'sudo apt -y install btrfs-tools'));
+
+        return install($params);
+
+    }catch(Exception $e){
+        throw new bException('btrfs_install(): Failed', $e);
+    }
+}
+
+
+
+/*
+ * Defragment the specified BTRFS path
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package btrfs
+ * @see btrfs_install()
+ * @see date_convert() Used to convert the sitemap entry dates
+ * @note: This function can be executed on remote servers by specifying $params[server]
+ * @version 1.27.0: Added function and documentation
+ *
+ * @param params $params The btrfs defragment parameters
+ * @params string $params[path]
+ * @params null mixed $params[server]
+ * @params null mixed $params[verbose]
+ * @return array The output lines from the "btrfs filesystem defrag" command
+ */
+function btrfs_defragment($params){
+    try{
+        array_ensure($params, 'verbose,path,server');
+
+        if($params['verbose'] or VERBOSE){
+            $verbose = '-v';
+
+        }else{
+            $verbose = '';
+        }
+
+        $results = servers_exec($params['server'], 'btrfs filesystem defragment '.$version.' "'.$params['path'].'"');
+        return $results;
+
+    }catch(Exception $e){
+        throw new bException('btrfs_defragment(): Failed', $e);
+    }
+}
+
+
+
+/*
+ * Create a BTRFS subvolume at the specified path
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package btrfs
+ * @see btrfs_snapshot_subvolume()
+ * @note: This function can be executed on remote servers by specifying $params[server]
+ * @version 1.27.0: Added function and documentation
+ *
+ * @param params $params The btrfs defragment parameters
+ * @params string $params[path]
+ * @params null mixed $params[server]
+ * @params null mixed $params[verbose]
+ * @return array The output lines from the "btrfs subvolume create" command
+ */
+function btrfs_create_subvolume($params){
+    try{
+
+    }catch(Exception $e){
+        throw new bException('btrfs_create_subvolume(): Failed', $e);
+    }
+}
+
+
+
+/*
+ * Create a BTRFS subvolume snapshot at the specified path
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package btrfs
+ * @see btrfs_create_subvolume()
+ * @note: This function can be executed on remote servers by specifying $params[server]
+ * @version 1.27.0: Added function and documentation
+ *
+ * @param params $params The btrfs defragment parameters
+ * @params string $params[path]
+ * @params null mixed $params[server]
+ * @params null mixed $params[verbose]
+ * @return array The output lines from the "btrfs subvolume snapshot" command
+ */
+function btrfs_snapshot_subvolume($params){
+    try{
+
+    }catch(Exception $e){
+        throw new bException('btrfs_snapshot_subvolume(): Failed', $e);
+    }
+}
+?>
