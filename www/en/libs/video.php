279493439c568cdab518f636809826240a265d86
Sven Olaf Oostenbrink <sven@capmega.com>
2019-02-04 19:32:05 -0600

    Rewrote video library
    Complete rewrite of library

--- www/en/libs/video.php
+++ www/en/libs/video.php
@@ -1,30 +1,66 @@
 <?php
+/*
+ * Video library
+ *
+ * This library contains various video management functions
+ *
+ * @author Sven Oostenbrink <support@capmega.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright 2019 Capmega <license@capmega.com>
+ * @category Function reference
+ * @package video
+ */
 
+
+
+/*
+ * Initialize the library, automatically executed by libs_load()
+ *
+ * NOTE: This function is executed automatically by the load_libs() function and does not need to be called manually
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package video
+ *
+ * @return void
+ */
 function video_library_init(){
     try{
         if(!safe_exec('which ffmpeg')){
-            throw new bException(tr('video: ffmpeg module not installed, run this command on your server: sudo apt update && sudo apt install ffmpeg libav-tools x264 x265;'), 'not_available');
+            throw new bException(tr('video_library_init(): ffmpeg module not installed, run this command on your server: sudo apt update && sudo apt install ffmpeg libav-tools x264 x265;'), 'not_available');
         }
-
-        load_libs('file');
 
     }catch(Exception $e){
         throw new bException('video_library_init(): Failed', $e);
     }
 }
 
-function get_video_thumbnail($video_path, $size='50x50'){
-    try{
-        $output_path = PUBTMP;
-        if (!file_exists($output_path)) {
-            mkdir($output_path);
-        }
-        $output_path .= substr(md5(strval(round(microtime(true) * 1000))), 0, 6) . '.jpeg';
 
-        $complete_command = "ffmpeg -i {$video_path} -deinterlace -an -ss 00:00:01 -t 00:00:02 -s {$size} -r 1 -y -vcodec mjpeg -f mjpeg {$output_path} 2>&1";
-        safe_exec($complete_command, 0);
-        return $output_path;
+
+/*
+ * Generates a thumbnail for the specified video file and returns the filename
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package video
+ *
+ * @params string $video_file The video file from which a thumbnail must be made
+ * @params string $size The required size of the thumbnail in XxY format
+ * @return string The generated thumbnail file
+ */
+function video_get_thumbnail($video_file, $size = '50x50'){
+    try{
+        $output_file = file_temp(false);
+        $command     = 'ffmpeg -i {'.$video_file.'} -deinterlace -an -ss 00:00:01 -t 00:00:02 -s {'.$size.'} -r 1 -y -vcodec mjpeg -f mjpeg {'.$output_file.'} 2>&1 >> '.ROOT.'data/log/video_thumbnail';
+
+        safe_exec($command);
+        return $output_file;
+
     }catch(Exception $e){
-        throw new bException('get_video_thumbnail(): Failed', $e);
+        throw new bException('video_get_thumbnail(): Failed', $e);
     }
 }
