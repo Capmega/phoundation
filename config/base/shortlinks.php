4dcc228e7773b63a938cae2971a26105586f545e
Sven Olaf Oostenbrink <sven@capmega.com>
2019-02-04 19:31:00 -0600

    Setup default configuration for shortlinks library

--- config/base/shortlinks.php
+++ config/base/shortlinks.php
@@ -1,3 +1,16 @@
 <?php
-$_CONFIG['shortlink']['google']['account'] = 'rbravo1403:fp00OWeV';
+/*
+ * SHORTLINKS LIBRARY CONFIGURATION FILE
+ *
+ * @author Sven Oostenbrink <support@capmega.com>,
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Configuration
+ * @package shortlinks
+ */
+$_CONFIG['shortlinks']                                                          = array('default' => 'internal',
+
+                                                                                        'providers' => array('internal' => array('account' => ''),
+                                                                                                             'bitly'    => array('account' => ''),
+                                                                                                             'google'   => array('account' => '')));
 ?>
