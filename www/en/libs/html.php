2c9046e7d22a5839a329360090796815f5cc11f5
Sven Olaf Oostenbrink <sven@capmega.com>
2019-02-06 18:37:05 -0600

    Improved html library
    Improved documentation header

--- www/en/libs/html.php
+++ www/en/libs/html.php
@@ -2,8 +2,11 @@
 /*
  * HTML library, containing all sorts of HTML functions
  *
+ * @author Sven Oostenbrink <support@capmega.com>
  * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
  * @copyright 2019 Capmega <license@capmega.com>
+ * @category Function reference
+ * @package html
  */
 
 
