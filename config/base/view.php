4a747ab1ed6c20067d5e6a6d052b2388601665af
Sven Olaf Oostenbrink <sven@capmega.com>
2019-02-23 00:39:13 -0600

    Added new library (and script) view
    The view library contains functions to view files like images, videos and pdfs
    
    Added view(): Will view any given file that is supported
    
    Added view_image(); Will view an image
    
    Added view_image_feh(): Will view an image using the feh image viewer

---
+++ config/base/view.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * VIEW CONFIGURATION FILE
+ *
+ * @author Sven Oostenbrink <support@capmega.com>,
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Configuration
+ * @package view
+ */
+$_CONFIG['view']                                                                = array('images' => array('default' => 'feh'),
+
+                                                                                        'videos' => array('default' => 'mplayer'),
+
+                                                                                        'pdf'    => array('default' => 'okular'));
+?>
