4abb6ea4d18d6b1b5f15c0c01238f37733f347b2
Sven Olaf Oostenbrink <sven@capmega.com>
2019-02-23 00:43:42 -0600

    Added drivers library
    The drivers library will be a drivers management library that can automatically install drivers for supported devices

---
+++ www/en/libs/drivers.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * Drivers library
+ *
+ * This library can manage hardware device drivers
+ *
+ * @author Sven Oostenbrink <support@capmega.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright 2019 Capmega <license@capmega.com>
+ * @category Function reference
+ * @package drivers
+ */
+
+
+
+/*
+ * Get and setup the driver for the specified device type, brand and model
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package drivers
+ * @version 2.4.10: Added function and documentation
+ *
+ * @param string $type
+ * @param string $brand
+ * @param string $model
+ * @return void
+ */
+function drivers_setup($type, $brand, $model, $server = null){
+    try{
+        cli_only();
+        load_libs('linux');
+
+        $brand = strtolower($brand);
+        $model = strtolower($model);
+
+        switch($type){
+            case 'printer':
+                // FALLTHROUGH
+            case 'scanner':
+                switch($brand){
+                    case 'brother':
+                        /*
+                         * Brother MFC L-8900 CDW scanner / printer combo
+                         * Download the generic driver file
+                         */
+                        $file  = linux_download($server, 'https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.1-1.gz');
+                        $file  = linux_unzip($server, $file).'linux-brprinter-installer-2.2.1-1';
+
+                        /*
+                         * Sanitize the model
+                         */
+                        $model = strtoupper($model);
+                        $model = str_replace(' ', '', $model);
+
+                        chmod($file, 0750);
+                        servers_exec($server, $file.' -f '.$model, null, true, 'passthru');
+                        break;
+
+                    default:
+                        throw new BException(tr('drivers_setup(): Unknown brand ":brand" for device type ":type" specified', array(':type' => $type, ':brand' => $brand)), 'unknown');
+                }
+
+            default:
+                throw new BException(tr('drivers_setup(): Unknown device type ":type" specified', array(':type' => $type)), 'unknown');
+        }
+
+    }catch(Exception $e){
+        throw new BException(tr('drivers_setup(): Failed'), $e);
+    }
+}
+?>
