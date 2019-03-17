362b19c9c0cfce605a98bb58c910188737cbb015
Sven Olaf Oostenbrink <sven@capmega.com>
2019-03-16 20:03:21 -0600

    Added a template configuration file

---
+++ config/base/template.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * EMPTY CONFIGURATION FILE
+ *
+ * @author Sven Oostenbrink <support@capmega.com>,
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Configuration
+ * @package
+ */
+$_CONFIG['']                                                                    = array('foo' => '',
+                                                                                        'bar' => true);                 // Blah
+?>
