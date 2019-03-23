bf18f89590a3231d1cc0e7dd82d7f5989fa2bb2c
Sven Olaf Oostenbrink <sven@capmega.com>
2019-03-22 18:20:06 -0600

    Deleted garbage

--- production_radius.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $_CONFIG['radius']['driver']        = 'freeradius';
-    $_CONFIG['radius']['mac']           = 'uppercase';
-    $_CONFIG['radius']['secret']        = 'nf34xxla242sa1';
-    $_CONFIG['radius']['server']        = 'radius.capmega.com';
-
-?>
