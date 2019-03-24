c35f171db831dfaedc8ee70f5c87dcf8ac391b49
Sven Olaf Oostenbrink <sven@capmega.com>
2019-03-23 19:39:44 -0600

    Fixed various 2.5.44 init file issues

--- init/framework/2.5.44.php
+++ init/framework/2.5.44.php
@@ -7,7 +7,9 @@ sql_query('ALTER TABLE `notifications_methods` MODIFY `method` ENUM("email", "sm
 /*
  * Add required tables for calendars library
  */
+sql_query('DROP TABLE IF EXISTS `calendars_events_notifications`');
 sql_query('DROP TABLE IF EXISTS `calendars_events_participants`');
+sql_query('DROP TABLE IF EXISTS `calendars_events_links`');
 sql_query('DROP TABLE IF EXISTS `calendars_events`');
 sql_query('DROP TABLE IF EXISTS `calendars`');
 
@@ -46,7 +48,7 @@ sql_query('CREATE TABLE `calendars_events` (`id`           INT(11)       NOT NUL
                                             `createdby`    INT(11)           NULL,
                                             `meta_id`      INT(11)       NOT NULL,
                                             `status`       VARCHAR(16)       NULL,
-                                            `calendars_id` INT(11)       NOT NULL,
+                                            `calendars_id` INT(11)           NULL,
                                             `documents_id` INT(11)           NULL,
                                             `name`         VARCHAR(32)   NOT NULL,
                                             `seoname`      VARCHAR(32)   NOT NULL,
@@ -58,17 +60,20 @@ sql_query('CREATE TABLE `calendars_events` (`id`           INT(11)       NOT NUL
                                                     KEY `createdby`    (`createdby`),
                                                     KEY `status`       (`status`),
                                                     KEY `calendars_id` (`calendars_id`),
+                                                    KEY `documents_id` (`documents_id`),
 
-                                            CONSTRAINT `fk_calendars_events_meta_id`      FOREIGN KEY (`meta_id`)      REFERENCES `meta`      (`id`) ON DELETE RESTRICT,
-                                            CONSTRAINT `fk_calendars_events_createdby`    FOREIGN KEY (`createdby`)    REFERENCES `users`     (`id`) ON DELETE RESTRICT,
-                                            CONSTRAINT `fk_calendars_events_calendars_id` FOREIGN KEY (`calendars_id`) REFERENCES `calendars` (`id`) ON DELETE RESTRICT
+                                            CONSTRAINT `fk_calendars_events_meta_id`      FOREIGN KEY (`meta_id`)      REFERENCES `meta`              (`id`) ON DELETE RESTRICT,
+                                            CONSTRAINT `fk_calendars_events_createdby`    FOREIGN KEY (`createdby`)    REFERENCES `users`             (`id`) ON DELETE RESTRICT,
+                                            CONSTRAINT `fk_calendars_events_calendars_id` FOREIGN KEY (`calendars_id`) REFERENCES `calendars`         (`id`) ON DELETE RESTRICT,
+                                            CONSTRAINT `fk_calendars_events_documents_id` FOREIGN KEY (`documents_id`) REFERENCES `storage_documents` (`id`) ON DELETE RESTRICT
 
                                            ) ENGINE=InnoDB AUTO_INCREMENT='.$_CONFIG['db']['core']['autoincrement'].' DEFAULT CHARSET="'.$_CONFIG['db']['core']['charset'].'" COLLATE="'.$_CONFIG['db']['core']['collate'].'";');
 
 
 
+
 /*
- * Create events table
+ * Create events participants table
  */
 sql_query('CREATE TABLE `calendars_events_participants` (`id`              INT(11)       NOT NULL AUTO_INCREMENT,
                                                          `createdon`       TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -76,7 +81,9 @@ sql_query('CREATE TABLE `calendars_events_participants` (`id`              INT(1
                                                          `meta_id`         INT(11)       NOT NULL,
                                                          `status`          VARCHAR(16)       NULL,
                                                          `events_id`       INT(11)       NOT NULL,
-                                                         `participants_id` VARCHAR(32)       NULL,
+                                                         `calendars_id`    INT(11)           NULL,
+                                                         `participants_id` INT(11)           NULL,
+                                                         `email`           VARCHAR(128)      NULL,
                                                          `description`     VARCHAR(2040)     NULL,
 
                                                          PRIMARY KEY `id`              (`id`),
@@ -86,10 +93,12 @@ sql_query('CREATE TABLE `calendars_events_participants` (`id`              INT(1
                                                                  KEY `status`          (`status`),
                                                                  KEY `events_id`       (`events_id`),
                                                                  KEY `participants_id` (`participants_id`),
+                                                                 KEY `calendars_id`    (`calendars_id`),
 
                                                          CONSTRAINT `fk_calendars_events_participants_meta_id`         FOREIGN KEY (`meta_id`)         REFERENCES `meta`             (`id`) ON DELETE RESTRICT,
                                                          CONSTRAINT `fk_calendars_events_participants_createdby`       FOREIGN KEY (`createdby`)       REFERENCES `users`            (`id`) ON DELETE RESTRICT,
                                                          CONSTRAINT `fk_calendars_events_participants_events_id`       FOREIGN KEY (`events_id`)       REFERENCES `calendars_events` (`id`) ON DELETE RESTRICT,
+                                                         CONSTRAINT `fk_calendars_events_participants_calendars_id`    FOREIGN KEY (`calendars_id`)    REFERENCES `calendars`        (`id`) ON DELETE RESTRICT,
                                                          CONSTRAINT `fk_calendars_events_participants_participants_id` FOREIGN KEY (`participants_id`) REFERENCES `users`            (`id`) ON DELETE RESTRICT
 
                                                         ) ENGINE=InnoDB AUTO_INCREMENT='.$_CONFIG['db']['core']['autoincrement'].' DEFAULT CHARSET="'.$_CONFIG['db']['core']['charset'].'" COLLATE="'.$_CONFIG['db']['core']['collate'].'";');
@@ -107,10 +116,6 @@ sql_query('CREATE TABLE `calendars_events_notifications` (`id`        INT(11)   
                                                           `events_id` INT(11)       NOT NULL,
                                                           `method`    ENUM("email", "sms", "desktop", "hangouts", "irc", "jabber", "push", "pushover", "prowl", "matrix", "whatsapp", "signal", "skype", "slack", "telegram", "twitter", "api") NOT NULL,
                                                           `before`    INT(11)       NOT NULL,
-
-
-
-
 
                                                           PRIMARY KEY `id`        (`id`),
                                                                   KEY `meta_id`   (`meta_id`),
