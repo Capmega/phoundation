5073b8ce5e6247576e61ad49b56d283a08371b9e
Camilo Rodriguez Cruz <crodriguez@capmega.com>
2019-03-25 12:13:28 -0600

    updated base sso config

--- config/base/sso.php
+++ config/base/sso.php
@@ -1,12 +1,4 @@
 <?php
 // SSO configuration
-$_CONFIG['sso']                                                                 = array('cache_config' => 86400,
-                                                                                        'facebook'     => false,
-                                                                                        'google'       => false,
-                                                                                        'linkedin'     => false,
-                                                                                        'microsoft'    => false,
-                                                                                        'paypal'       => false,
-                                                                                        'reddit'       => false,
-                                                                                        'twitter'      => false,
-                                                                                        'yandex'       => false);
+$_CONFIG['sso']['cache_config'] = 86400;
 ?>
