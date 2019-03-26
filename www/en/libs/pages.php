1a4391ee7fb48ee24b4fe09866c68115adbc727a
Sven Olaf Oostenbrink <sven@capmega.com>
2019-03-25 20:52:02 -0600

    v2.5.53: Added pages library
    Pages library contains functions to manage the available web pages

---
+++ www/en/libs/pages.php
@@ -0,0 +1,199 @@
+<?php
+/*
+ * Pages library
+ *
+ * This library contains functions to manage the various web pages on this system, list them, get details, etc
+ *
+ * @author Sven Oostenbrink <support@capmega.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright 2019 Capmega <license@capmega.com>
+ * @category Function reference
+ * @package pages
+ */
+
+
+
+/*
+ * Return a list of all available pages filtered by
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @pages Function reference
+ * @see sql_simple_list()
+ * @package pages
+ * @version 2.5.50: Added function and documentation
+ *
+ * @return mixed The list of available pages
+ */
+function pages_list($params){
+    try{
+        array_ensure($params, 'filters');
+
+        $retval = array();
+        $pages  = scandir(ROOT.'www/en/');
+
+        $params['filters'] = array_force($params['filters']);
+
+        foreach($pages as $id => $page){
+            $page = strtolower($page);
+
+            if(substr($page, -4, 4) != '.php'){
+                continue;
+            }
+
+            foreach($params['filters'] as $key => $value){
+                if(($key === 'name') and !str_exists($page, $value)){
+                    continue;
+                }
+
+                $retval[$page] = array('name'        => str_runtil(basename($page), '.php'),
+                                       'package'     => '',
+                                       'description' => '');
+            }
+        }
+
+        return $retval;
+
+    }catch(Exception $e){
+        throw new BException(tr('pages_list(): Failed'), $e);
+    }
+}
+
+
+
+/*
+ * Return HTML for a pages select box
+ *
+ * This function will generate HTML for an HTML select box using html_select() and fill it with the available pages
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @pages Function reference
+ * @package pages
+ * @see sql_simple_get()
+ * @version 2.5.38: Added function and documentation
+ *
+ * @param array $params The parameters required
+ * @param $params name
+ * @param $params class
+ * @param $params empty
+ * @param $params none
+ * @param $params selected
+ * @param $params parents_id
+ * @param $params status
+ * @param $params orderby
+ * @param $params resource
+ * @return string HTML for a pages select box within the specified parameters
+ */
+function pages_select($params = null){
+    try{
+        array_ensure($params);
+        array_default($params, 'name'      , 'seopages');
+        array_default($params, 'class'     , 'form-control');
+        array_default($params, 'selected'  , null);
+        array_default($params, 'autosubmit', true);
+        array_default($params, 'parents_id', null);
+        array_default($params, 'status'    , null);
+        array_default($params, 'remove'    , null);
+        array_default($params, 'empty'     , tr('No pages available'));
+        array_default($params, 'none'      , tr('Select a pages'));
+        array_default($params, 'orderby'   , '`name`');
+
+        $execute = array();
+
+        if($params['remove']){
+            if(count(array_force($params['remove'])) == 1){
+                /*
+                 * Filter out only one entry
+                 */
+                $where[] = ' `id` != :id ';
+                $execute[':id'] = $params['remove'];
+
+            }else{
+                /*
+                 * Filter out multiple entries
+                 */
+                $in      = sql_in(array_force($params['remove']));
+                $where[] = ' `id` NOT IN ('.implode(', ', array_keys($in)).') ';
+                $execute = array_merge($execute, $in);
+            }
+        }
+
+        if($params['parents_id']){
+            $where[] = ' `parents_id` = :parents_id ';
+            $execute[':parents_id'] = $params['parents_id'];
+
+        }else{
+            $where[] = ' `parents_id` IS NULL ';
+        }
+
+        if($params['status'] !== false){
+            $where[] = ' `status` '.sql_is($params['status'], ':status');
+            $execute[':status'] = $params['status'];
+        }
+
+        if(empty($where)){
+            $where = '';
+
+        }else{
+            $where = ' WHERE '.implode(' AND ', $where).' ';
+        }
+
+        $query              = 'SELECT `seoname`, `name` FROM `pages` '.$where.' ORDER BY `name`';
+        $params['resource'] = sql_query($query, $execute);
+        $retval             = html_select($params);
+
+        return $retval;
+
+    }catch(Exception $e){
+        throw new BException(tr('pages_select(): Failed'), $e);
+    }
+}
+
+
+
+/*
+ * SUB HEADER TEXT
+ *
+ * PARAGRAPH
+ *
+ * PARAGRAPH
+ *
+ * PARAGRAPH
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package pages
+ * @see pages_install()
+ * @see date_convert() Used to convert the sitemap entry dates
+ * @table: `pages`
+ * @note: This is a note
+ * @version 2.5.38: Added function and documentation
+ * @example [Title]
+ * code
+ * $result = pages_function(array('foo' => 'bar'));
+ * showdie($result);
+ * /code
+ *
+ * This would return
+ * code
+ * Foo...bar
+ * /code
+ *
+ * @param params $params A parameters array
+ * @param string $params[foo]
+ * @param string $params[bar]
+ * @return string The result
+ */
+function pages_function($params){
+    try{
+
+    }catch(Exception $e){
+        throw new BException(tr('pages_function(): Failed'), $e);
+    }
+}
+?>
--- www/en/libs/system.php
+++ www/en/libs/system.php
@@ -16,7 +16,7 @@
 /*
  * Framework version
  */
-define('FRAMEWORKCODEVERSION', '2.5.52');
+define('FRAMEWORKCODEVERSION', '2.5.53');
 define('PHP_MINIMUM_VERSION' , '5.5.9');
 
 
