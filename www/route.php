c2a79fec32df670c992a6d94c59c99cb8e683673
Sven Olaf Oostenbrink <sven@capmega.com>
2019-03-26 23:18:10 -0600

    Improved routing example

--- www/route.php
+++ www/route.php
@@ -14,10 +14,25 @@
 $veryverbose = true;
 require_once(__DIR__.'/en/libs/route.php');
 
-route('/^[a-z]{2}\/$/'                    , 'http' , 'index');                                                                   // Show index page
-route('/^[a-z]{2}\/ajax\/([a-z\/]+).php$/', 'ajax' , '$1', 'Q');                                                                 // Show pages with page name in URL
-route(''                                  , ''     , ':PROTOCOL:DOMAIN/:REQUESTED_LANGUAGE/', 'R301');                           // Main page has to redirect to a language page
-route('/^[a-z]{2}\/admin\/?$/'            , ''     , ':PROTOCOL:DOMAIN/:REQUESTED_LANGUAGE/admin/index.html', 'R301');           // /en/admin/ has to go to /en/admin/index.html
-route('/^[a-z]{2}\/admin\/([a-z]+).html$/', 'admin', '$1', 'Q');                                                                 // Show pages with page name in URL
-route('/^admin\/?$/'                      , ''     , ':PROTOCOL:DOMAIN/:REQUESTED_LANGUAGE/admin/index.html', 'R301');           // /admin/ has to go to /en/admin/index.html
+/*
+ * Setup URL translations map
+ */
+route('map', array('es' => array('conferencias' => 'conferences',
+                                 'portafolio'   => 'portfolio',
+                                 'servicios'    => 'services',
+                                 'nosotros'     => 'about'),
+
+                   'nl' => array('conferenties' => 'conferences',
+                                 'portefeuille' => 'portfolio',
+                                 'diensten'     => 'services',
+                                 'over-ons'     => 'about')));
+
+route('/^([a-z]{2})\/$/'                                    , '$1/index.php'                                         , '');                 // Show index page
+route('/^([a-z]{2})\/([-a-z]+).html$/'                      , '$1/$2.php'                                            , '');                 // Show pages with page name in URL
+route('/^([a-z]{2})\/(?!admin)([a-z]+)\/([a-z0-9-]+).html$/', '$1/$2.php?item=$3'                                    , '');                 // Show pages with page/section name in URL
+route(''                                                    , ':PROTOCOL:DOMAIN/:REQUESTED_LANGUAGE/'                , 'R301');             // Main page has to redirect to a language page
+route('/^([a-z]{2})\/(admin\/)?ajax\/([a-z\/]+).php$/'      , '$1/$2ajax/$3.php'                                     , 'Q');                // Redirect to admin ajax pages
+route('/^([a-z]{2})\/admin\/?$/'                            , ':PROTOCOL:DOMAIN/:REQUESTED_LANGUAGE/admin/index.html', 'R301');             // /en/admin/ has to go to /en/admin/index.html
+route('/^([a-z]{2})\/admin\/([a-z-]+).html$/'               , '$1/admin/$2.php'                                      , 'Q');                // Show admin pages with page name in URL
+route('/^admin\/?$/'                                        , ':PROTOCOL:DOMAIN/:REQUESTED_LANGUAGE/admin/index.html', 'R301');             // /admin/ has to go to /en/admin/index.html
 ?>
