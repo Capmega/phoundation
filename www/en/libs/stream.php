9f7ad64ca3e2177aa46f4f6de79c6caf2a5b972e
Sven Olaf Oostenbrink <sven@capmega.com>
2019-03-31 03:18:52 -0600

    v2.5.86: Implemented data streaming library
    Added steaming video support (taken from https://codesamplez.com/programming/php-html5-video-streaming-tutorial and rewritten for use in phoundation), awaiting audio streaming support

--- www/en/libs/stream.php
+++ www/en/libs/stream.php
@@ -29,6 +29,7 @@
  */
 function stream_library_init(){
     try{
+        load_config('stream');
 
     }catch(Exception $e){
         throw new BException(tr('stream_library_init(): Failed'), $e);
@@ -38,111 +39,352 @@ function stream_library_init(){
 
 
 /*
- * SUB HEADER TEXT
+ * Stream a video file
  *
  * @author Sven Olaf Oostenbrink <sven@capmega.com>
  * @copyright Copyright (c) 2018 Capmega
  * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
  * @category Function reference
  * @package stream
- * @see stream_install()
- * @see date_convert() Used to convert the sitemap entry dates
- * @table: `template`
- * @note: This is a note
  * @version 2.5.92: Added function and documentation
+ * @note Inspiration for this function taken from https://codesamplez.com/programming/php-html5-video-streaming-tutorial
+ * @see http://codesamplez.com/programming/php-html5-video-streaming-tutorial
+ * @see stream_open()
+ * @see stream_close()
+ * @see stream_video()
+ * @see stream_audio()
  *
  * @param params $params The streaming parameters array
  */
-function stream_download($params){
+function stream($params){
     try{
-under_construction();
+        array_ensure($params, 'file,mimetype');
+
         /*
-         * set appropriate headers for attachment or streamed file
+         * Open the file to be streamed and determine its mimetype to know what
+         * sub functions to use
          */
-// :BUG: Possible bug, the Content-Disposition: attachment header is already specified in the last line, while with $stream it would be inline?
-        if($stream){
-            header('Content-Disposition: inline;');
+        stream_open($params);
 
-        }else{
-            header('Content-Disposition: attachment; filename="'.$file_name.'"');
-        }
+        $mimetype = mime_content_type($params['file']);
 
-        // set the mime type based on extension, add yours if needed.
-        $ctype_default = 'application/octet-stream';
-        $content_types = array('exe' => 'application/octet-stream',
-                               'zip' => 'application/zip',
-                               'mp3' => 'audio/mpeg',
-                               'mpg' => 'video/mpeg',
-                               'avi' => 'video/x-msvideo');
-
-        $ctype = isset($content_types[$file_ext]) ? $content_types[$file_ext] : $ctype_default;
-        header("Content-Type: ".$ctype);
-
-        //check if http_range is sent by browser (or download manager)
-        if(isset($_SERVER['HTTP_RANGE'])){
-            list($size_unit, $range_orig) = explode('=', $_SERVER['HTTP_RANGE'], 2);
-
-            if($size_unit == 'bytes'){
+        if($params['mimetype']){
+            /*
+             * Ensure the specified file maches the mimetype
+             */
+            if($params['strict']){
                 /*
-                 * multiple ranges could be specified at the same time, but for simplicity only serve the first range
-                 * http://tools.ietf.org/id/draft-ietf-http-range-retrieval-00.txt
+                 * Match the entire mimetype
                  */
-                list($range, $extra_ranges) = explode(',', $range_orig, 2);
+                if($mimetype !== $params['mimetype']){
+                    throw new BException(tr('stream(): Specified file ":file" failed strict mimetype check. If has mimetype ":has" while ":requested" was requested', array(':has' => $mimetype, ':mimetype' => $params['mimetype'])), 'not-authorized');
+                }
 
             }else{
-                $range = '';
-                header('HTTP/1.1 416 Requested Range Not Satisfiable');
-                throw new BException(tr('file_http_download(): Unknown size_unit ":size_unit" specified, please ensure its "bytes"', array(':size_unit' => $size_unit)), 'not-exist');
-            }
-
-        }else{
-            $range = '';
-        }
-
-        //figure out download piece from range (if set)
-        list($seek_start, $seek_end) = explode('-', $range, 2);
-
-        //set start and end based on range (if set), else set defaults
-        //also check for invalid ranges.
-        $seek_end   = (empty($seek_end)) ? ($file_size - 1) : min(abs(intval($seek_end)), ($file_size - 1));
-        $seek_start = (empty($seek_start) or ($seek_end < abs(intval($seek_start)))) ? 0 : max(abs(intval($seek_start)), 0);
-
-        //Only send partial content header if downloading a piece of the file (IE workaround)
-        if($seek_start or ($seek_end < ($file_size - 1))){
-            header('HTTP/1.1 206 Partial Content');
-            header('Content-Range: bytes '.$seek_start.'-'.$seek_end.'/'.$file_size);
-            header('Content-Length: '.($seek_end - $seek_start + 1));
-
-        }else{
-            header('Content-Length: '.$file_size);
-        }
-
-        header('Accept-Ranges: bytes');
-
-        set_time_limit(0);
-        fseek($file, $seek_start);
-
-        /*
-         * Download file to client
-         */
-        while(!feof($file)){
-            print(fread($file, 8912));
-            ob_flush();
-            flush();
-
-            if(connection_status()){
-                fclose($file);
-                exit;
+                if(str_until($mimetype, '/') !== str_until($params['mimetype'], '/')){
+                    throw new BException(tr('stream(): Specified file ":file" failed lax mimetype check. If has mimetype ":has" while ":requested" was requested', array(':has' => str_until($mimetype, '/'), ':mimetype' => str_until($params['mimetype'], '/'))), 'not-authorized');
+                }
             }
         }
 
-        /*
-         * file download was a success
-         */
-        fclose($file);
+        switch(str_until($mimetype, '/')){
+            case 'audio':
+                return stream_audio($params);
+
+            case 'video':
+                return stream_video($params);
+
+            throw new BException(tr('stream(): Unsupported mimetype ":mimetype" encounered', array(':mimetype' => str_until($mimetype, '/'))), 'unsupported');
+        }
 
     }catch(Exception $e){
-        throw new BException(tr('stream_function(): Failed'), $e);
+        throw new BException(tr('stream(): Failed'), $e);
+    }
+}
+
+
+
+/*
+ * Open the file for streaming
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package stream
+ * @version 2.5.86: Added function and documentation
+ * @see stream_close()
+ *
+ * @param params $params A parameters array
+ * @return void
+ */
+function stream_open(&$params){
+    try{
+        if(empty($params['file'])){
+            throw new BException(tr('stream_open(): No file specified'), 'not-specified');
+        }
+
+        if(!file_exists($params['file'])){
+            throw new BException(tr('stream_open(): Specified file ":file" does not exist', array(':file' => $params['file'])), 'not-exist');
+        }
+
+        if(!is_readable($params['file'])){
+            throw new BException(tr('stream_open(): Specified file ":file" is not readable', array(':file' => $params['file'])), 'not-readable');
+        }
+
+        $params['resource'] = fopen($this->path, 'rb');
+
+        if(!$params['resource']){
+            throw new BException(tr('stream_open(): Failed to open file ":file" for streaming', array(':file' => $params['file'])), $e);
+        }
+
+        $params['start']     = 0;
+        $params['size']      = filesize($params['file']);
+        $params['end']       = $params['size'];
+        $params['filemtime'] = filemtime($params['file']);
+
+    }catch(Exception $e){
+        throw new BException(tr('stream_open(): Failed'), $e);
+    }
+}
+
+
+
+/*
+ * Closes the stream file
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package stream
+ * @version 2.5.92: Added function and documentation
+ * @note Inspiration for this function taken from https://codesamplez.com/programming/php-html5-video-streaming-tutorial
+ * @see http://codesamplez.com/programming/php-html5-video-streaming-tutorial
+ * @see stream_open()
+ *
+ * @param params $params The streaming parameters array
+ */
+function stream_close($params){
+    try{
+        array_ensure($params);
+
+        array_default($params, 'die', true);
+
+        if(empty($params['resource'])){
+            throw new BException(tr('stream_close(): No video file resource opened. Please open one first using stream_open(), or just use stream()'), 'not-specified');
+        }
+
+        fclose($params['resource']);
+
+        if(empty($params['die'])){
+            die();
+        }
+
+    }catch(Exception $e){
+        throw new BException(tr('stream_close(): Failed'), $e);
+    }
+}
+
+
+
+/*
+ * Stream a audio file
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package stream
+ * @version 2.5.92: Added function and documentation
+ * @note Inspiration for this function taken from https://codesamplez.com/programming/php-html5-audio-streaming-tutorial
+ * @see http://codesamplez.com/programming/php-html5-audio-streaming-tutorial
+ *
+ * @param params $params The streaming parameters array
+ */
+function stream_audio($params){
+    try{
+under_construction('Audio streaming is still under construction');
+        stream_audio_send_headers($params);
+        stream_audio_send($params);
+        stream_end($params);
+
+    }catch(Exception $e){
+        throw new BException(tr('stream_audio(): Failed'), $e);
+    }
+}
+
+
+
+/*
+ * Stream a video file
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package stream
+ * @version 2.5.92: Added function and documentation
+ * @note Inspiration for this function taken from https://codesamplez.com/programming/php-html5-video-streaming-tutorial
+ * @see http://codesamplez.com/programming/php-html5-video-streaming-tutorial
+ *
+ * @param params $params The streaming parameters array
+ */
+function stream_video($params){
+    try{
+        stream_video_data_headers($params);
+        stream_video_data($params);
+        stream_end($params);
+
+    }catch(Exception $e){
+        throw new BException(tr('stream_video(): Failed'), $e);
+    }
+}
+
+
+
+/*
+ * Send the video stream HTTP headers
+ *
+ * @author Rana
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package stream
+ * @version 2.5.92: Added function and documentation
+ * @note Inspiration for this function taken from https://codesamplez.com/programming/php-html5-video-streaming-tutorial
+ * @see http://codesamplez.com/programming/php-html5-video-streaming-tutorial
+ *
+ * @param params $params The streaming parameters array
+ * return void
+ */
+function stream_video_data_headers($params){
+    try{
+        array_ensure($params);
+
+        if(empty($params['resource'])){
+            throw new BException(tr('stream_video_data_headers(): No video file resource opened. Please open one first using stream_open(), or just use stream()'), 'not-specified');
+        }
+
+        if(empty($params['file'])){
+            throw new BException(tr('stream_video_data_headers(): No video file specified'), 'not-specified');
+        }
+
+        /*
+         * Clean the output buffer and start sending HTTP headers
+         */
+        ob_get_clean();
+
+        header('Content-Type: video/mp4');
+        header('Cache-Control: max-age=2592000, public');
+        header('Expires: '.gmdate('D, d M Y H:i:s', time() + 2592000) . ' GMT');
+        header('Last-Modified: '.gmdate('D, d M Y H:i:s', $params['filemtime']) . ' GMT' );
+
+        header('Accept-Ranges: 0-'.$params['end']);
+
+        if(empty($_SERVER['HTTP_RANGE'])){
+            header('Content-Length: '.$params['size']);
+            return;
+        }
+
+        $c_start = $params['start'];
+        $c_end   = $params['end'];
+
+        list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+
+        if(strpos($range, ',') !== false){
+            header('HTTP/1.1 416 Requested Range Not Satisfiable');
+            header('Content-Range: bytes '.$params['start'].'-'.$params['end'].'/'.$params['size']);
+            die();
+        }
+
+        if($range == '-'){
+            $c_start = $params['size'] - substr($range, 1);
+
+        }else{
+            $range   = explode('-', $range);
+            $c_start = $range[0];
+            $c_end   = (isset($range[1]) and (is_numeric($range[1])) ? $range[1] : $c_end);
+        }
+
+        $c_end = (($c_end > $params['end']) ? $params['end'] : $c_end);
+
+        if(($c_start > $c_end) || ($c_start > $params['size'] - 1) || ($c_end >= $params['size'])){
+            header('HTTP/1.1 416 Requested Range Not Satisfiable');
+            header('Content-Range: bytes '.$params['start'].'-'.$params['end'].'/'.$params['size']);
+            die();
+        }
+
+        $params['start'] = $c_start;
+        $params['end']   = $c_end;
+
+        $length = $params['end'] - $params['start'] + 1;
+        fseek($this->stream, $params['start']);
+
+        header('HTTP/1.1 206 Partial Content');
+        header('Content-Length: '.$length);
+        header('Content-Range: bytes '.$params['start'].'-'.$params['end'].'/'.$params['size']);
+
+    }catch(Exception $e){
+        throw new BException(tr('stream_video_data_headers(): Failed'), $e);
+    }
+}
+
+
+
+/*
+ * Stream the video file data
+ *
+ * @author Rana
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package stream
+ * @version 2.5.92: Added function and documentation
+ * @note Inspiration for this function taken from https://codesamplez.com/programming/php-html5-video-streaming-tutorial
+ * @see http://codesamplez.com/programming/php-html5-video-streaming-tutorial
+ *
+ * @param params $params The streaming parameters array
+ */
+function stream_video_data($params){
+    global $_CONFIG;
+
+    try{
+        array_ensure($params);
+        array_default($params, 'buffer', $_CONFIG['stream']['buffer']);
+
+
+        if(empty($params['resource'])){
+            throw new BException(tr('stream_video_data_headers(): No video file resource opened. Please open one first using stream_open(), or just use stream()'), 'not-specified');
+        }
+
+        if(empty($params['file'])){
+            throw new BException(tr('stream_video_data_headers(): No video file specified'), 'not-specified');
+        }
+
+        /*
+         * Disable timelimit because we might be streaming for a while
+         */
+        set_time_limit(0);
+        $i = $params['start'];
+
+        while(!feof($params['resource']) && $i <= $params['end']){
+            $bytesToRead = $params['buffer'];
+
+            if(($i + $bytesToRead) > $params['end']){
+                $bytesToRead = $params['end'] - $i + 1;
+            }
+
+            echo fread($params['resource'], $bytesToRead);
+            flush();
+
+            $i += $bytesToRead;
+        }
+
+    }catch(Exception $e){
+        throw new BException(tr('stream_video_data(): Failed'), $e);
     }
 }
 ?>
