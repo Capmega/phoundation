3fee80a07f71ab52d939c181cce35f0afe5ebc6b
Sven Olaf Oostenbrink <sven@capmega.com>
2019-04-02 11:19:08 -0600

    Deleted garbage

--- init/framework/2.5.103.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
- * Fix missing indices on dictionary table
- */
-sql_index_exists('dictionary', 'language', 'ALTER TABLE `dictionary` ADD INDEX `language` (`language`)');
-sql_index_exists('dictionary', 'status'  , 'ALTER TABLE `dictionary` ADD INDEX `status`   (`status`)');
-?>
