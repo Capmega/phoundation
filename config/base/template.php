d2d0db9e60e76edf394f6bed9b50f36045b268a8
Sven Olaf Oostenbrink <sven@capmega.com>
2019-04-01 21:33:37 -0600

    Improved template configuration file
    Improved documentation

--- config/base/template.php
+++ config/base/template.php
@@ -1,13 +1,24 @@
 <?php
 /*
- * EMPTY CONFIGURATION FILE
+ * PHOUNDATION FILE LIBRARY CONFIGURATION FILE
+ *
+ * DO NOT MODIFY THIS FILE!
+ *
+ * This file contains default valuesthat may be overwritten when you perform a
+ * system update! Always update the following configuration files if you need to
+ * make configuration changes
+ *
+ * production_empty.php
+ * ENVIRONMENT_empty.php (Where ENVIRONMENT is the environment for which you wish to change the configuration)
+ *
+ * ALL CONFIGURATION ENTRIES ARE ORDERED ALPHABETICALLY, ONLY "debug" IS ON TOP FOR CONVENIENCE
  *
  * @author Sven Oostenbrink <support@capmega.com>,
  * @copyright Copyright (c) 2018 Capmega
  * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
  * @category Configuration
- * @package
+ * @package template
  */
-$_CONFIG['']                                                                    = array('foo' => '',
-                                                                                        'bar' => true);                 // Blah
+$_CONFIG['template']                                                            = array('bar' => '',
+                                                                                        'foo' => true);                 // Blah
 ?>
