2d4ace47a965dcdd71e7497291ebddd7c5bd26e6
Sven Olaf Oostenbrink <sven@capmega.com>
2019-04-01 18:50:20 -0600

    Improved default template routing table

--- www/route.php
+++ www/route.php
@@ -87,12 +87,25 @@ route('map', array('es' => array('conferencias' => 'conferences',
                                  'diensten'     => 'services',
                                  'over-ons'     => 'about')));
 
+/*
+ * Front end pages
+ */
 route('/^([a-z]{2})\/$/'                                    , '$1/index.php'                                         , '');                 // Show index page
 route('/^([a-z]{2})\/([-a-z]+).html$/'                      , '$1/$2.php'                                            , '');                 // Show pages with page name in URL
 route('/^([a-z]{2})\/(?!admin)([a-z]+)\/([a-z0-9-]+).html$/', '$1/$2.php?item=$3'                                    , '');                 // Show pages with page/section name in URL
 route(''                                                    , ':PROTOCOL:DOMAIN/:REQUESTED_LANGUAGE/'                , 'R301');             // Main page has to redirect to a language page
+
+/*
+ * Admin pages
+ */
 route('/^([a-z]{2})\/(admin\/)?ajax\/([a-z\/]+).php$/'      , '$1/$2ajax/$3.php'                                     , 'Q');                // Redirect to admin ajax pages
 route('/^([a-z]{2})\/admin\/?$/'                            , ':PROTOCOL:DOMAIN/:REQUESTED_LANGUAGE/admin/index.html', 'R301');             // /en/admin/ has to go to /en/admin/index.html
 route('/^([a-z]{2})\/admin\/([a-z-]+).html$/'               , '$1/admin/$2.php'                                      , 'Q');                // Show admin pages with page name in URL
 route('/^admin\/?$/'                                        , ':PROTOCOL:DOMAIN/:REQUESTED_LANGUAGE/admin/index.html', 'R301');             // /admin/ has to go to /en/admin/index.html
+
+/*
+ * System files / downloadable files
+ */
+route('/([a-z]{2})\/([a-z]+\.(?:xml)|(?:txt))$/'            , '$1'                                                   , '');                 // System files like sitemap.xml, robot.txt, etc.
+route('/([a-z]{2})\/files\/(.+)$/'                          , '$1'                                                   , '');                 // Downloadable files
 ?>
