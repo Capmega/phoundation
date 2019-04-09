47628e6ce00a93562df2661fd80e552148f1d4f9
Sven Olaf Oostenbrink <sven@capmega.com>
2019-04-08 20:18:29 -0500

    Fixed init 2.5.120
    Fixed syntax error

--- init/framework/2.5.120.php
+++ init/framework/2.5.120.php
@@ -2,10 +2,10 @@
 /*
  * Fix missing seonames column in database_accounts table
  */
-sql_query('ALTER TABLE `databases` CHANGE COLUMN `name` VARCHAR(32) NOT NULL');
+sql_query('ALTER TABLE `databases` MODIFY COLUMN `name` VARCHAR(32) NOT NULL');
 
 sql_column_exists('databases', 'seoname', '!ALTER TABLE `databases` ADD COLUMN     `seoname` VARCHAR(32) NOT NULL AFTER `name`');
 sql_index_exists ('databases', 'seoname', '!ALTER TABLE `databases` ADD UNIQUE KEY `seoname` (`seoname`)');
 
-sql_query('ALTER TABLE `databases` CHANGE COLUMN `replication_status` `replication_status` ENUM("enabled", "enabling", "pausing", "resuming", "preparing", "paused", "disabled", "error") NULL DEFAULT "disabled"');
+sql_query('ALTER TABLE `databases` MODIFY COLUMN `replication_status` ENUM("enabled", "enabling", "pausing", "resuming", "preparing", "paused", "disabled", "error") NULL DEFAULT "disabled"');
 ?>
