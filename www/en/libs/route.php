80cdf6cf7cd08f52a2a31838616d91c952ebd047
Sven Olaf Oostenbrink <sven@capmega.com>
2019-05-16 15:40:17 -0500

    Improved library route
    Improved route(): The init log entry now shows the client IP as well

--- www/en/libs/route.php
+++ www/en/libs/route.php
@@ -110,7 +110,7 @@ function route($regex, $target, $flags = null){
          */
         if(!$init){
             $init = true;
-            log_file(tr('Processing ":domain" routes for ":type" type request ":url"', array(':domain' => $_CONFIG['domain'], ':type' => $type, ':url' => $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'])), 'route', 'white');
+            log_file(tr('Processing ":domain" routes for ":type" type request ":url" from client ":client"', array(':domain' => $_CONFIG['domain'], ':type' => $type, ':url' => $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'], ':client' => $_SERVER['REMOTE_ADDR'])), 'route', 'white');
             register_shutdown('route_404', null);
         }
 
