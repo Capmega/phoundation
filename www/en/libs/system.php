a94a4d3761965d016fe194c41ad311e974f6b474
Sven Olaf Oostenbrink <sven@capmega.com>
2019-07-09 13:36:58 -0500

    v2.7.4: Modified system library
    Modified array_force(): If $source is null or empty string "" an empty array will be returned. For any other scalar "empty" value an array with an entry will be returned

--- www/en/libs/system.php
+++ www/en/libs/system.php
@@ -16,7 +16,7 @@
 /*
  * Framework version
  */
-define('FRAMEWORKCODEVERSION', '2.7.3');
+define('FRAMEWORKCODEVERSION', '2.7.4');
 define('PHP_MINIMUM_VERSION' , '5.5.9');
 
 
@@ -4522,7 +4522,7 @@ function array_ensure(&$source, $keys = '', $default_value = null, $trim_existin
  */
 function array_force($source, $separator = ','){
     try{
-        if(!$source){
+        if(($source === '') or ($source === null)){
             return array();
         }
 
