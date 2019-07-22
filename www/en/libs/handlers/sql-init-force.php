df78ba5f70014b9df4d31da1d0259e9edfedf5f7
Sven Olaf Oostenbrink <sven@capmega.com>
2019-07-22 15:28:13 -0500

    Fixed sql library
    Fixed sql_init_force(): Crashed on various bugs

--- www/en/libs/handlers/sql-init-force.php
+++ www/en/libs/handlers/sql-init-force.php
@@ -1,26 +1,27 @@
 <?php
-    global $_CONFIG;
+global $_CONFIG;
+
+/*
+ * We're doing a forced init from shell. Forced init will
+ * basically set database version to 0 BY DROPPING THE FUCKER SO BE CAREFUL!
+ *
+ * Forced init is NOT allowed on production (for obvious safety reasons, doh!)
+ */
+if($_CONFIG['production']){
+    throw new BException(tr('sql_init(): For safety reasons, init force is NOT allowed on production environment! Please drop the database using "./scripts/base/init drop" or in the mysql console with "DROP DATABASE :db"and continue with a standard init', array(':db' => $_CONFIG['db'][$connector]['db'])), 'forcedenied');
+}
+
+if(!str_is_version(FORCE)){
+    if(!is_bool(FORCE)){
+        throw new BException(tr('sql_init(): Invalid "force" sub parameter ":force" specified. "force" can only be followed by a valid init version number', array(':force' => FORCE)), 'invalidforce');
+    }
 
     /*
-     * We're doing a forced init from shell. Forced init will
-     * basically set database version to 0 BY DROPPING THE FUCKER SO BE CAREFUL!
-     *
-     * Forced init is NOT allowed on production (for obvious safety reasons, doh!)
+     * Dump database, and recreate it
      */
-    if($_CONFIG['production']){
-        throw new BException('sql_init(): For safety reasons, init force is NOT allowed on production environment! Please drop the database using "./scripts/base/init drop" or in the mysql console with "DROP DATABASE \''.str_log($_CONFIG['db'][$connector]['db']).'\'"and continue with a standard init', 'forcedenied');
-    }
+    sql_connect($connector);
 
-    if(!str_is_version(FORCE)){
-        if(!is_bool(FORCE)){
-            throw new BException('sql_init(): Invalid "force" sub parameter "'.str_log(FORCE).'" specified. "force" can only be followed by a valid init version number', 'invalidforce');
-        }
-
-        /*
-         * Dump database, and recreate it
-         */
-        $core->sql[$connector]->query('DROP   DATABASE IF EXISTS `'.$_CONFIG['db'][$connector]['db'].'`');
-        $core->sql[$connector]->query('CREATE DATABASE           `'.$_CONFIG['db'][$connector]['db'].'` DEFAULT CHARSET="'.$_CONFIG['db'][$connector]['charset'].'" COLLATE="'.$_CONFIG['db'][$connector]['collate'].'";');
-        $core->sql[$connector]->query('USE                       `'.$_CONFIG['db'][$connector]['db'].'`');
-    }
-?>
+    $core->sql['core']->query('DROP   DATABASE IF EXISTS `'.$_CONFIG['db']['core']['db'].'`');
+    $core->sql['core']->query('CREATE DATABASE           `'.$_CONFIG['db']['core']['db'].'` DEFAULT CHARSET="'.$_CONFIG['db']['core']['charset'].'" COLLATE="'.$_CONFIG['db']['core']['collate'].'";');
+    $core->sql['core']->query('USE                       `'.$_CONFIG['db']['core']['db'].'`');
+}
