9363ec8c6884bab8fab1ed00e98d4a417eae85b5
Sven Olaf Oostenbrink <sven@capmega.com>
2019-07-29 16:29:17 -0500

    v2.7.24: Fixed html library
    Fixed html_script(): Crash when trying to write internal javascript to external cache files in readonly directories

--- www/en/libs/html.php
+++ www/en/libs/html.php
@@ -2080,8 +2080,12 @@ function html_script($script, $event = 'dom_content', $extra = null, $type = 'te
             }
 
             if(!file_exists($file.'.js')){
-                log_file(tr('Writing internal javascript to externally cached file ":file"', array(':file' => $file.'.js')), 'html-script', 'cyan');
-                file_put_contents($file.'.js', $retval);
+                $file .= '.js';
+
+                file_execute_mode(dirname($file), 0770, function() use ($file, $retval){
+                    log_file(tr('Writing internal javascript to externally cached file ":file"', array(':file' => $file.'.js')), 'html-script', 'cyan');
+                    file_put_contents($file.'.js', $retval);
+                });
             }
 
             /*
