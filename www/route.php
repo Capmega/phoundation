a69af4ecbc76e186d5b035e27292580cd69b91f3
Sven Olaf Oostenbrink <sven@capmega.com>
2019-11-12 19:01:32 -0600

    Improved documentation

--- www/route.php
+++ www/route.php
@@ -11,14 +11,22 @@
  * The second argument is the page you wish to execute and the variables that should be sent to it. If your regular expression captured variables, you may use these variables here. If the page name itself is a variable, then route() will try to find that page, and execute it if it exists
  *
  * The third argument is a list (CSV string or array) with flags. Current allowed flags are:
- * Q Allow queries to pass through. If NOT specified, and the URL contains queries, the URL will NOT match!
- * R301 Redirect to the specified page argument using HTTP 301
- * R302 Redirect to the specified page argument using HTTP 302
- * P The request must be POST to match
- * G The request must be GET to match
- * C Use URL cloaking. A cloaked URL is basically a random string that the route() function can look up in the `cloak` table. domain() and its related functions will generate these URL's automatically. See the "url" library, and domain() and related functions for more information
- *
- * The $verbose and $veryverbose variables here are to set the system in VERBOSE or VERYVERBOSE mode, but ONLY if the system runs in debug mode. The former will add extra log output in the data/log files, the latter will add LOADS of extra log data in the data/log files, so please use with care and only if you cannot resolve the problem
+ * A                Process the target as an attachement (i.e. Send the file so that the browser client can download it)
+ * B                Block. Return absolutely nothing
+ * C                Use URL cloaking. A cloaked URL is basically a random string that the route() function can look up in the `cloak` table. domain() and its related functions will generate these URL's automatically. See the "url" library, and domain() and related functions for more information
+ * D                Add HTTP_HOST to the REQUEST_URI before applying the match
+ * G                The request must be GET to match
+ * H                If the routing rule matches, the router will add a *POSSIBLE HACK ATTEMPT DETECTED* log entry for later processing
+ * L                Disable language map requirements for this specific URL (Use this with non language URLs on a multi lingual site)
+ * P                The request must be POST to match
+ * M                Add queries into the REQUEST_URI before applying the match, autmatically implies Q
+ * N                Do not check for permanent routing rules
+ * Q                Allow queries to pass through. If NOT specified, and the URL contains queries, the URL will NOT match!
+ * QKEY;KEY=ACTION  Is a ; separated string containing query keys that are allowed, and if specified, what action must be taken when encountered
+ * R301             Redirect to the specified page argument using HTTP 301
+ * R302             Redirect to the specified page argument using HTTP 302
+ * S$SECONDS$       Store the specified rule for this IP and apply it for $SECONDS$ amount of seconds. $SECONDS$ is optional, and defaults to 86400 seconds (1 day). This works well to auto 404 IP's that are doing naughty things for at least a day
+ * X$PATHS$         Restrict access to the specified dot-comma separated $PATHS$ list. $PATHS is optional and defaults to ROOT.'www,'.ROOT.'data/content/downloads'
  *
  * The translation map helps route() to detect URL's where the language is native. For example; http://phoundation.org/about.html and http://phoundation.org/nosotros.html should both route to about.php, and maybe you wish to add multiple languages for this. The routing table basically says what static words should be translated to their native language counterparts. The mapped_domain() function use this table as well when generating URL's. See mapped_domain() for more information
  * @author Sven Olaf Oostenbrink <sven@capmega.com>
