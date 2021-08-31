92f348cc54ebd0f29d410d1080ff2ae0fce682ef
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2021-08-30 19:09:14 -0500

    WIP moving more core functionalities to new Phoundation class architecture

--- Phoundation/Core/Debug.php
+++ Phoundation/Core/Debug.php
@@ -5,18 +5,57 @@
  */
 class Debug {
     /**
-     *
+     * Returns the filename from where this call was made
      *
      * @param int $trace
-     * @return mixed|string|null
+     * @return string
      */
-    public static function currentFile(int $trace = 0) {
+    public static function currentFile(int $trace = 0): string
+    {
         $backtrace = debug_backtrace();
 
         if(!isset($backtrace[$trace + 1])){
-            return 'no_current_file';
+            return '-';
         }
 
-        return isset_get($backtrace[$trace + 1]['file']);
+        return isset_get($backtrace[$trace + 1]['file'], '-');
+    }
+
+
+
+    /**
+     * Returns the line number from where this call was made
+     *
+     * @param int $trace
+     * @return string
+     */
+    public static function currentFunction(int $trace = 0): string
+    {
+        $backtrace = debug_backtrace();
+
+        if(!isset($backtrace[$trace + 1])){
+            return -1;
+        }
+
+        return isset_get($backtrace[$trace + 1]['function'], '-');
+    }
+
+
+
+    /**
+     * Returns the line number from where this call was made
+     *
+     * @param int $trace
+     * @return int
+     */
+    public static function currentLine(int $trace = 0): int
+    {
+        $backtrace = debug_backtrace();
+
+        if(!isset($backtrace[$trace + 1])){
+            return -1;
+        }
+
+        return isset_get($backtrace[$trace + 1]['line'], -1);
     }
 }
