2a143cb3aef74f7e93b221b04088d89b8c5040b0
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2021-08-30 19:14:16 -0500

    WIP moving more core functionalities to new Phoundation class architecture

--- Phoundation/Core/Debug.php
+++ Phoundation/Core/Debug.php
@@ -5,6 +5,37 @@
  */
 class Debug {
     /**
+     * Returns a backtrace
+     *
+     * @return array
+     */
+    public static function backtrace(string $filters = 'args', bool $skip_own = true): array
+    {
+        if(!debug()){
+            return array();
+        }
+
+        $filters = array_force($filters);
+        $trace   = array();
+
+        foreach(debug_backtrace() as $key => $value){
+            if($skip_own and ($key <= 1)){
+                continue;
+            }
+
+            foreach($filters as $filter){
+                unset($value[$filter]);
+            }
+
+            $trace[] = $value;
+        }
+
+        return $trace;
+    }
+
+
+
+    /**
      * Returns the filename from where this call was made
      *
      * @param int $trace
