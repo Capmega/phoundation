f37eeef2f063eeae98a769aab3020941fd5d256a
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2022-12-05 21:02:03 -0800

    Improved Core\Strings class
    Added Strings::uuid() Returns an RFC 4122 compliant version 4 UUID

--- Phoundation/Core/Strings.php
+++ Phoundation/Core/Strings.php
@@ -1693,4 +1693,28 @@ class Strings
 
         return $return;
     }
+
+
+
+    /**
+     * Generates and returns an RFC 4122 compliant Version 4 UUID
+     *
+     * @note Taken from https://www.uuidgenerator.net/dev-corner/php
+     * @param $data
+     * @return string|void
+     * @throws Exception
+     */
+    public static function uuid($data = null) {
+        // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+        $data = $data ?? random_bytes(16);
+        assert(strlen($data) == 16);
+
+        // Set version to 0100
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        // Set bits 6-7 to 10
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+        // Output the 36 character UUID.
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
 }
