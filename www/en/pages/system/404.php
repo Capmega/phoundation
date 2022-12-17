f553e92a78547e28ed94264870b1ad80a0e5e6c2
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2022-12-16 18:41:23 -0800

    Fixed 404 page
    Added missing page meta data

--- www/en/pages/system/404.php
+++ www/en/pages/system/404.php
@@ -1,3 +1,8 @@
+<?php
+
+use Phoundation\Web\WebPage;
+use Templates\AdminLte\Components\BreadCrumbs;
+?>
 <div class="container">
     <div class="d-flex justify-content-center align-items-center" style="height: 100vh">
         <div class="text-center">
@@ -5,4 +10,12 @@
         </div>
     </div>
 </div>
-
+<?php
+// Set page meta data
+WebPage::setPageTitle(tr('404 - Page not found'));
+WebPage::setHeaderTitle('');
+WebPage::setDescription(tr('404 - Page not found: The page you requested does not (or no longer) exist on this server'));
+WebPage::setBreadCrumbs(BreadCrumbs::new([
+    '/' => tr('Home'),
+    ''  => tr('404')
+]));
