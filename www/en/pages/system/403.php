9c64d300724d8ac5f75a2242536d4ec9523e01a5
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2022-12-19 13:19:35 -0800

    Added 403 system page
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

---
+++ www/en/pages/system/403.php
@@ -0,0 +1,21 @@
+<?php
+
+use Phoundation\Web\WebPage;
+use Templates\AdminLte\Components\BreadCrumbs;
+?>
+<div class="container">
+    <div class="d-flex justify-content-center align-items-center" style="height: 100vh">
+        <div class="text-center">
+            <h1>(403) Forbidden!</h1>
+        </div>
+    </div>
+</div>
+<?php
+// Set page meta data
+WebPage::setPageTitle(tr('403 - Forbidden'));
+WebPage::setHeaderTitle('');
+WebPage::setDescription(tr('403 - Forbidden: You do not have access to the requested resource on this server'));
+WebPage::setBreadCrumbs(BreadCrumbs::new([
+    '/' => tr('Home'),
+    ''  => tr('403')
+]));
