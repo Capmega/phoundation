d5821492e0a10047efc519f152881777956b0e71
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2022-12-19 20:09:36 -0800

    Cleaned tests

--- www/en/pages/index.php
+++ www/en/pages/index.php
@@ -12,7 +12,7 @@ use Templates\AdminLte\Layouts\GridColumn;
 // Build the page
 $card = Card::new()
     ->setTitle(tr('This is a test!'))
-    ->setBody('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam leo nisl, iaculis nec est quis, dapibus commodo mi. Nunc dui metus, ultricies ac vestibulum sit amet, rutrum tristique est. Aenean et consectetur sem. Mauris non scelerisque urna, in efficitur nibh. Nulla facilisi. Ut tempor ligula fringilla nibh commodo, sed scelerisque erat posuere. Aenean lobortis volutpat sem, eu tincidunt neque hendrerit non. Nunc maximus ante et arcu maximus maximus. Ut vitae leo et arcu condimentum pellentesque sed et diam. Mauris ut sapien porttitor, pharetra erat quis, suscipit leo. Vestibulum a libero vitae quam tempor aliquam. Proin ultrices nisl in ante aliquam, at posuere arcu luctus. Nulla iaculis porttitor sem eu dignissim.');
+    ->setContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam leo nisl, iaculis nec est quis, dapibus commodo mi. Nunc dui metus, ultricies ac vestibulum sit amet, rutrum tristique est. Aenean et consectetur sem. Mauris non scelerisque urna, in efficitur nibh. Nulla facilisi. Ut tempor ligula fringilla nibh commodo, sed scelerisque erat posuere. Aenean lobortis volutpat sem, eu tincidunt neque hendrerit non. Nunc maximus ante et arcu maximus maximus. Ut vitae leo et arcu condimentum pellentesque sed et diam. Mauris ut sapien porttitor, pharetra erat quis, suscipit leo. Vestibulum a libero vitae quam tempor aliquam. Proin ultrices nisl in ante aliquam, at posuere arcu luctus. Nulla iaculis porttitor sem eu dignissim.');
 
 $column = GridColumn::new()
     ->setSize(3)
