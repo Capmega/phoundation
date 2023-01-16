74fa5a67f505a9576e0f2e02f3bdea082770c71e
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-01-15 23:06:26 -0800

    Implementing Language classes
    Implemented Languages::import()

--- Phoundation/Core/Locale/Language/Language.php
+++ Phoundation/Core/Locale/Language/Language.php
@@ -2,14 +2,219 @@
 
 namespace Phoundation\Core\Locale\Language;
 
-class Language extends \Phoundation\Data\DataEntry
+use Phoundation\Data\DataEntry;
+use Phoundation\Data\DataEntryNameDescription;
+
+
+/**
+ * Language class
+ *
+ *
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2022 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Core
+ */
+class Language extends DataEntry
 {
+    use DataEntryNameDescription;
+
+
+
+    /**
+     * Language class constructor
+     *
+     * @param int|string|null $identifier
+     */
+    public function __construct(int|string|null $identifier = null)
+    {
+        self::$entry_name    = 'language';
+        $this->table         = 'languages';
+        $this->unique_column = 'seo_name';
+
+        parent::__construct($identifier);
+    }
+
+
+
+    /**
+     * Returns the code_639_1 for this language
+     *
+     * @return string|null
+     */
+    public function getCode_639_1(): ?string
+    {
+        return $this->getDataValue('code_639_1');
+    }
+
+
+
+    /**
+     * Sets the code_639_1 for this language
+     *
+     * @param string|null $code_639_2_b
+     * @return static
+     */
+    public function setCode_639_1(?string $code_639_1): static
+    {
+        return $this->setDataValue('code_639_1', $code_639_1);
+    }
+
+
+
+    /**
+     * Returns the code_639_2_b for this language
+     *
+     * @return string|null
+     */
+    public function getCode_639_2_b(): ?string
+    {
+        return $this->getDataValue('code_639_2_b');
+    }
+
+
+
+    /**
+     * Sets the code_639_2_b for this language
+     *
+     * @param string|null $code_639_2_b
+     * @return static
+     */
+    public function setCode_639_2_b(?string $code_639_2_b): static
+    {
+        return $this->setDataValue('code_639_2_b', $code_639_2_b);
+    }
+
+
+
+    /**
+     * Returns the code_639_2_t for this language
+     *
+     * @return string|null
+     */
+    public function getCode_639_2_t(): ?string
+    {
+        return $this->getDataValue('code_639_2_t');
+    }
+
+
+
+    /**
+     * Sets the code_639_2_t for this language
+     *
+     * @param string|null $code_639_2_t
+     * @return static
+     */
+    public function setCode_639_2_t(?string $code_639_2_t): static
+    {
+        return $this->setDataValue('code_639_2_t', $code_639_2_t);
+    }
+
+
+
+    /**
+     * Returns the code_639_3 for this language
+     *
+     * @return string|null
+     */
+    public function getCode_639_3(): ?string
+    {
+        return $this->getDataValue('code_639_3');
+    }
+
+
+
+    /**
+     * Sets the code_639_3 for this language
+     *
+     * @param string|null $code_639_3
+     * @return static
+     */
+    public function setCode_639_3(?string $code_639_3): static
+    {
+        return $this->setDataValue('code_639_3', $code_639_3);
+    }
+
+
 
     /**
      * @inheritDoc
      */
     protected function setKeys(): void
     {
-        // TODO: Implement setKeys() method.
+        $this->keys = [
+            'id' => [
+                'disabled' => true,
+                'type'     => 'numeric',
+                'label'    => tr('Database ID')
+            ],
+            'created_by' => [
+                'disabled' => true,
+                'source'   => 'SELECT IFNULL(`username`, `email`) AS `username` FROM `accounts_users` WHERE `id` = :id',
+                'execute'  => 'id',
+                'label'    => tr('Created by')
+            ],
+            'created_on' => [
+                'disabled' => true,
+                'type'     => 'date',
+                'label'    => tr('Created on')
+            ],
+            'meta_id' => [
+                'disabled' => true,
+                'element'  => null, //Meta::new()->getHtmlTable(), // TODO implement
+                'label'    => tr('Meta information')
+            ],
+            'status' => [
+                'disabled' => true,
+                'default'  => tr('Ok'),
+                'label'    => tr('Status')
+            ],
+            'name' => [
+                'disabled'  => true,
+                'label'     => tr('Name')
+            ],
+            'seo_name' => [
+                'display'  => false,
+            ],
+            'code_639_1' => [
+                'disabled' => true,
+                'type'     => 'text',
+                'label'    => tr('ISO 639-1 code')
+            ],
+            'code_639_2_t' => [
+                'disabled' => true,
+                'type'     => 'text',
+                'label'    => tr('ISO 639-2/T code')
+            ],
+            'code_639_2_b' => [
+                'disabled' => true,
+                'type'     => 'text',
+                'label'    => tr('ISO 639-2/B code')
+            ],
+            'code_639_3' => [
+                'disabled' => true,
+                'type'     => 'text',
+                'label'    => tr('ISO 639-3 code')
+            ],
+            'description' => [
+                'label'   => tr('Description'),
+                'element' => 'text'
+            ]
+        ];
+
+        $this->form_keys = [
+            'id'            => 12,
+            'created_by'    => 6,
+            'created_on'    => 6,
+            'meta_id'       => 6,
+            'status'        => 6,
+            'name'          => 12,
+            'code_639_1'    => 6,
+            'code_639_2_t'  => 6,
+            'code_639_2_b'  => 6,
+            'code_639_3'    => 6,
+            'description'   => 12
+        ];
     }
 }
--- Phoundation/Core/Locale/Language/Languages.php
+++ Phoundation/Core/Locale/Language/Languages.php
@@ -2,7 +2,95 @@
 
 namespace Phoundation\Core\Locale\Language;
 
-class Languages
-{
+use Phoundation\Data\DataList;
+use Phoundation\Filesystem\File;
+use Phoundation\Filesystem\Filesystem;
+use Phoundation\Web\Http\Html\Components\Input\Select;
 
+
+
+/**
+ * Languages class
+ *
+ *
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2022 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Core
+ */
+class Languages extends DataList
+{
+    /**
+     * Import the content for the languages table from a data-source file
+     *
+     * @return void
+     */
+    public static function import(): void
+    {
+        $file = File::new(PATH_DATA . 'sources/languages/languages');
+        $h    = $file->open('r');
+
+        sql()->truncate('languages');
+
+        while($line = fgets($h, $file->getBufferSize())) {
+            // Parse the line
+            switch ($line[0]) {
+                case '#':
+                    // no break
+                case ';':
+                    // no break
+                case '//':
+                    continue 2;
+            }
+
+            $line = explode("\t", $line);
+
+            // Import the language data into a language object and save.
+            $language = Language::new();
+            $language->setName(isset_get($line[0]));
+            $language->setCode_639_1(isset_get($line[1]));
+            $language->setCode_639_2_t(isset_get($line[2]));
+            $language->setCode_639_2_b(isset_get($line[3]));
+            $language->setCode_639_3(isset_get($line[4]));
+            $language->setDescription(isset_get($line[5]));
+            $language->save();
+        }
+    }
+
+
+
+    /**
+     * Returns an HTML <select> object with all available languages
+     *
+     * @return Select
+     */
+    public static function getHtmlSelect(): Select
+    {
+        $select = Select::new();
+        $select->setSourceQuery('SELECT `code_639_1`, `name` FROM `languages` WHERE `status` IS NULL ORDER BY `name`');
+
+        return $select;
+    }
+
+
+
+    /**
+     * @param string|null $id_column
+     * @return $this
+     */
+    protected function load(?string $id_column = null): static
+    {
+        // TODO: Implement load() method.
+    }
+
+    protected function loadDetails(array|string|null $columns, array $filters = []): array
+    {
+        // TODO: Implement loadDetails() method.
+    }
+
+    public function save(): static
+    {
+        // TODO: Implement save() method.
+    }
 }
