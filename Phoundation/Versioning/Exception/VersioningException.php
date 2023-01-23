f04a8cd086865c37b3294f41e4a22e9cf7d7a0b1
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-01-22 16:04:53 -0800

    Added Versioning library
    First steps to get a Git class

---
+++ Phoundation/Versioning/Exception/VersioningException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Phoundation\Versioning\Exception;
+
+use Phoundation\Exception\Exception;
+
+
+
+/**
+ * Class VersioningException
+ *
+ * This is the standard exception for all Phoundation Core classes
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2022 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Versioning
+ */
+class VersioningException extends Exception
+{
+}
