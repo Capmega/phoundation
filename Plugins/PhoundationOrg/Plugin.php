fedb436ffb5a02e7753a7f138ecb179868dd9fb5
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-01-25 12:59:17 -0800

    Added PhoundationOrg class plugin

---
+++ Plugins/PhoundationOrg/Plugin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Plugins\PhoundationOrg;
+
+
+
+/**
+ * Class Plugin
+ *
+ *
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2022 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Plugins\PhoundationOrg
+ */
+class Plugin extends \Phoundation\Core\Plugin
+{
+
+}
