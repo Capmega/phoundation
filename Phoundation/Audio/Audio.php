8e863499a2c588f803f132deb1b06d2e5f8ca7ed
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-02-02 22:26:25 -0800

    Implemented Audio library
    Added Audio::play() Can play the specified audio file. If no absolute file was given, ROOT/data/audio will be assumed
    
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

--- Phoundation/Audio/Audio.php
+++ Phoundation/Audio/Audio.php
@@ -2,8 +2,13 @@
 
 namespace Phoundation\Audio;
 
+use Phoundation\Core\Log\Log;
+use Phoundation\Filesystem\Exception\FileNotExistException;
 use Phoundation\Filesystem\File;
+use Phoundation\Filesystem\Filesystem;
+use Phoundation\Processes\Exception\ProcessesException;
 use Phoundation\Processes\Process;
+use Throwable;
 
 
 /**
@@ -20,16 +25,24 @@ class Audio extends File
     /**
      * Play this audio file
      *
+     * @param bool $background
      * @return static
      */
-    public function play(): static
+    public function play(bool $background = false): static
     {
         try {
-            Process::new('aplay')
-                ->addArgument($this->file)
-                ->executeNoReturn();
-        } catch (Throwable $e) {
-showdie($e);
+            $this->file = Filesystem::absolute($this->file, PATH_ROOT . 'data/audio');
+            $process    = Process::new('mplayer')->addArgument($this->file);
+
+            if ($background) {
+                $process->executeBackground();
+            } else {
+                $process->executeNoReturn();
+            }
+
+        } catch (FileNotExistException|ProcessesException $e) {
+            Log::warning(tr('Failed to play the requested audio file because of the following exception'));
+            Log::warning($e->getMessage());
         }
 
         return $this;
