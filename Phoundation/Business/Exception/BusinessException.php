31d11a890b183bc35e6ac9fd9ceb46df24281c5a
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-02-14 20:19:38 -0800

    Implementing business management scripts
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

---
+++ Phoundation/Business/Exception/BusinessException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Phoundation\Business\Exception;
+
+use Phoundation\Exception\Exception;
+
+
+
+/**
+ * Class BusinessException
+ *
+ *
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2022 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Business
+ */
+class BusinessException extends Exception
+{
+}
