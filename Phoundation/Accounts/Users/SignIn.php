9368c2a17164eaddb0ea6045358f75c655fb0f06
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-03-22 14:30:00 -0700

    Improved Users library
    Implementing SignIn object, added data validation
    
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

--- Phoundation/Accounts/Users/SignIn.php
+++ Phoundation/Accounts/Users/SignIn.php
@@ -7,6 +7,9 @@ use Phoundation\Data\DataEntry\Traits\DataEntryIpAddress;
 use Phoundation\Data\DataEntry\Traits\DataEntryTimezone;
 use Phoundation\Data\DataEntry\Traits\DataEntryUserAgent;
 use Phoundation\Data\Traits\DataGeoIp;
+use Phoundation\Data\Validator\ArgvValidator;
+use Phoundation\Data\Validator\GetValidator;
+use Phoundation\Data\Validator\PostValidator;
 use Phoundation\Geo\Countries\Countries;
 use Phoundation\Geo\GeoIp\GeoIp;
 use Phoundation\Geo\Timezones\Timezones;
@@ -61,6 +64,33 @@ class SignIn extends DataEntry
             ->setGeoIp(GeoIp::detect($_SERVER['REMOTE_ADDR']));
     }
 
+
+    /**
+     * Validates the data contained in the validator object
+     *
+     * @param GetValidator|PostValidator|ArgvValidator $validator
+     * @param bool $no_arguments_left
+     * @param bool $modify
+     * @return array
+     */
+    protected function validate(ArgvValidator|PostValidator|GetValidator $validator, bool $no_arguments_left = false, bool $modify = false): array
+    {
+        $data = $validator
+            ->select($this->getAlternateValidationField('ip_address'), true)->hasMaxCharacters(16)
+            ->select($this->getAlternateValidationField('net_len'), true)->isOptional()->isNatural()->isLessThan(48)
+            ->select($this->getAlternateValidationField('ip_address_human'), true)->isIp()
+            ->select($this->getAlternateValidationField('user_agent'), true)->isOptional()->hasMaxCharacters(2048)
+            ->select($this->getAlternateValidationField('latitude'), true)->isOptional()->isLessThan(90)->isMoreThan(-90)
+            ->select($this->getAlternateValidationField('longitude'), true)->isOptional()->isLessThan(180)->isMoreThan(-180)
+            ->select($this->getAlternateValidationField('country'), true)->or('countries_id')->isName()->isQueryColumn('SELECT `name` FROM `geo_countries`  WHERE `name` = :name AND `status` IS NULL', [':name' => '$country'])
+            ->select($this->getAlternateValidationField('countries_id'), true)->or('country')->isId()->isQueryColumn  ('SELECT `id`   FROM `geo_countries`  WHERE `id`   = :id   AND `status` IS NULL', [':id'   => '$countries_id'])
+            ->select($this->getAlternateValidationField('timezone'), true)->or('timezones_id')->isName()->isQueryColumn('SELECT `name` FROM `geo_timezones` WHERE `name` = :name AND `status` IS NULL', [':name' => '$timezone'])
+            ->select($this->getAlternateValidationField('timezones_id'), true)->or('timezone')->isId()->isQueryColumn  ('SELECT `id`   FROM `geo_timezones` WHERE `id`   = :id   AND `status` IS NULL', [':id'   => '$timezones_id'])
+            ->noArgumentsLeft($no_arguments_left)
+            ->validate();
+
+        return $data;
+    }
 
 
     /**
