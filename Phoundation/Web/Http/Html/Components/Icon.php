9728e05f133e4d4c1d6c1348ac4b417861429a9e
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-04-29 12:16:07 -0700

    Improved Web Html library
    Added Icon, FontAwesome classes: used to create icons

---
+++ Phoundation/Web/Http/Html/Components/Icon.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Phoundation\Web\Http\Html\Components;
+
+use JetBrains\PhpStorm\ExpectedValues;
+
+
+/**
+ * Icon class
+ *
+ *
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2022 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Web
+ */
+class Icon extends Element
+{
+    /**
+     * The icoin being displayed
+     *
+     * @var string|null $icon
+     */
+    protected ?string $icon = null;
+
+
+    /**
+     * Returns the icon for this object
+     *
+     * @return string|null
+     */
+    public function getIcon(): ?string
+    {
+        return $this->icon;
+    }
+
+
+    /**
+     * Sets the icon for this object
+     *
+     * @param string|null $icon
+     * @param string $subclass
+     * @return static
+     */
+    public function setIcon(?string $icon, string $subclass = ''): static
+    {
+        $this->addClass($subclass);
+        $this->icon = $icon;
+        return $this;
+    }
+
+
+    /**
+     * Form class constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setElement('i');
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function render(): ?string
+    {
+        $this->addClass($this->icon);
+        return parent::render();
+    }
+}
