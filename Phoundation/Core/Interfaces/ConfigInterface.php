a9c58cff99547ec99f2378a4b7d455cde25588f2
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-08-07 11:50:24 -0700

    Added Config interface
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

---
+++ Phoundation/Core/Interfaces/ConfigInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Phoundation\Core\Interfaces;
+
+
+/**
+ * Class Config
+ *
+ * This class contains the methods to read, write and manage configuration options. Default configuration values are
+ * specified in the classes themselves whereas users can add configuration sections in the YAML file
+ * PATH_ROOT/config/ENVIRONMENT/CLASSNAME and this class will apply those values.
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2023 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Core
+ */
+interface ConfigInterface
+{
+}
