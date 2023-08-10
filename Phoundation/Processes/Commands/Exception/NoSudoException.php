eb7875afb86d5ced92311026d762c9ad56ec05b2
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-08-09 19:06:39 -0700

    Added Processes NoSudoException
    Thrown when sudo privileges are not available for the specified command
    
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

---
+++ Phoundation/Processes/Commands/Exception/NoSudoException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phoundation\Processes\Commands\Exception;
+
+
+/**
+ * Class NoSudoException
+ *
+ * This exception is thrown if the current process owner has no sudo privileges for the specified command
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2023 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Processes
+ */
+class NoSudoException extends CommandsException
+{
+}
