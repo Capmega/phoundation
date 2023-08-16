0ff006cb199562ff65bad0ab2c61631757810eed
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-08-15 20:49:10 -0700

    Added comments
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

--- www/pages/index.php
+++ www/pages/index.php
@@ -5,6 +5,17 @@ use Phoundation\Web\Page;
 use Phoundation\Web\Http\Html\Components\BreadCrumbs;
 
 
+/**
+ * Index page
+ *
+ * This is the main page of the entire site
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2023 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Web
+ */
+
 
 // Set page meta data
 Page::setPageTitle(tr('Dashboard'));
