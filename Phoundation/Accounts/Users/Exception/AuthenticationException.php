22f725993ac660a8ed654051c2548d8312591000
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-08-15 21:06:38 -0700

    Fixed 401/403 Exceptions
    AuthenticationException requires redirect now
    
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

--- Phoundation/Accounts/Users/Exception/AuthenticationException.php
+++ Phoundation/Accounts/Users/Exception/AuthenticationException.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Phoundation\Accounts\Users\Exception;
 
+use Phoundation\Exception\AccessDeniedException;
 use Throwable;
 
 
@@ -19,9 +20,47 @@ use Throwable;
  */
 class AuthenticationException extends UsersException
 {
+    /**
+     * The new target that should be executed because of this access denied
+     *
+     * @var string|int|null
+     */
+    protected string|int|null $new_target;
+
+
+    /**
+     * AuthenticationException class constructor
+     *
+     * @param Throwable|array|string|null $messages
+     * @param Throwable|null $previous
+     */
     public function __construct(Throwable|array|string|null $messages, ?Throwable $previous = null)
     {
         parent::__construct($messages, $previous);
         $this->makeWarning();
+    }
+
+
+    /**
+     * Returns the new target
+     *
+     * @return string|int|null
+     */
+    public function getNewTarget(): string|int|null
+    {
+        return $this->new_target;
+    }
+
+
+    /**
+     * Sets the new target
+     *
+     * @param string|int|null $new_target
+     * @return static
+     */
+    public function setNewTarget(string|int|null $new_target): static
+    {
+        $this->new_target = $new_target;
+        return $this;
     }
 }
--- Phoundation/Exception/AccessDeniedException.php
+++ Phoundation/Exception/AccessDeniedException.php
@@ -14,34 +14,4 @@ namespace Phoundation\Exception;
  */
 class AccessDeniedException extends Exception
 {
-    /**
-     * The new target that should be executed because of this access denied
-     *
-     * @var string|int|null
-     */
-    protected string|int|null $new_target;
-
-
-    /**
-     * Returns the new target
-     *
-     * @return string|int|null
-     */
-    public function getNewTarget(): string|int|null
-    {
-        return $this->new_target;
-    }
-
-
-    /**
-     * Sets the new target
-     *
-     * @param string|int|null $new_target
-     * @return AccessDeniedException
-     */
-    public function setNewTarget(string|int|null $new_target): static
-    {
-        $this->new_target = $new_target;
-        return $this;
-    }
 }
