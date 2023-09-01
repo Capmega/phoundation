6ad446238ba08a435a5a7afa660cc01eb735c015
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-08-31 19:01:39 -0700

    Improved Core library
    Added Enum EnumLibraryType
    
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

---
+++ Phoundation/Core/Enums/EnumLibraryType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Phoundation\Core\Enums;
+
+use Phoundation\Core\Enums\Interfaces\EnumLibraryTypeInterface;
+
+
+/**
+ * Enum EnumLibraryType
+ *
+ * This enum contains the three different type of libraries that can exist: System, Plugin, Template
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2023 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Developer
+ */
+enum EnumLibraryType: string implements EnumLibraryTypeInterface
+{
+    case system   = 'Phoundation/';
+    case plugin   = 'Plugins/';
+    case template = 'Templates/';
+}
