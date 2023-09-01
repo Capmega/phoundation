d598be3439cd1f0bd33992083d99ce0f1c6b904d
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-08-31 19:01:57 -0700

    Added template library to easily create future libraries
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

---
+++ Phoundation/.TemplateLibrary/.Library/Updates.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace :type:name;
+
+
+/**
+ * Updates class
+ *
+ * This is the Init class for the :name library
+ *
+ * @see \Phoundation\Core\Libraries\Updates
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2023 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package :type:name
+ */
+class Updates extends \Phoundation\Core\Libraries\Updates
+{
+    /**
+     * The current version for this library
+     *
+     * @return string
+     */
+    public function version(): string
+    {
+        return '0.0.1';
+    }
+
+
+    /**
+     * Returns the library description
+     *
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        // TODO Write a description for this library
+        return tr('');
+    }
+
+
+    /**
+     * The list of version updates available for this library
+     *
+     * @return void
+     */
+    public function updates(): void
+    {
+        $this->addUpdate('0.0.1', function () {
+        });
+    }
+}
