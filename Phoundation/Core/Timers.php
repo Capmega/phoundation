1419d4ddb4b845d51e0b50bbff1ef6ca7d86330f
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-09-14 06:36:16 -0700

    Rewrote Core Timer classes
    Can now correctly time multiple processes and return results for statistics and debugging
    
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

--- Phoundation/Core/Timers.php
+++ Phoundation/Core/Timers.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Phoundation\Core;
 
-
+use Phoundation\Core\Exception\TimerException;
+use Phoundation\Core\Interfaces\TimerInterface;
+use Phoundation\Core\Interfaces\TimersInterface;
 use Phoundation\Exception\OutOfBoundsException;
 
 
@@ -19,7 +21,7 @@ use Phoundation\Exception\OutOfBoundsException;
  * @package Phoundation\Core
  * @see Timer
  */
-class Timers
+class Timers implements TimersInterface
 {
     /**
      * All running timer objects are stored here
@@ -32,54 +34,152 @@ class Timers
     /**
      * Add the specified timer object to the timer register
      *
-     * @param Timer $timer
-     * @return Timer
+     * @param TimerInterface $timer
+     * @return TimerInterface
      */
-    public static function add(Timer $timer): Timer
+    public static function add(string $group, TimerInterface $timer): TimerInterface
     {
-        if (array_key_exists($timer->getName(), static::$timers)) {
-            throw new OutOfBoundsException('The specified timer ":name" already exists', [':name' => $timer->getName()]);
-        }
-
-        static::$timers[$timer->getName()] = $timer;
-
+        static::ensureGroup($group);
+        static::$timers[$group][] = $timer;
         return $timer;
     }
 
 
     /**
-     * Returns the specified timer
+     * Returns a new timer
      *
-     * @param string $name
-     * @return Timer
+     * @param string $group
+     * @param string $label
+     * @param bool $start
+     * @return TimerInterface
      */
-    public static function get(string $name): Timer
+    public static function new(string $group, string $label = '', bool $start = true): TimerInterface
     {
-        if (!array_key_exists($name, static::$timers)) {
-            // Create the timer
-            return Timer::create($name);
-        }
-
-        return static::$timers[$name];
+        static::ensureGroup($group);
+        return static::$timers[$group][] = Timer::new($label, $start);
     }
 
 
     /**
-     * Returns the specified timer
+     * Returns all the timers under the specified group
      *
-     * @param string $name
-     * @return Timer
+     * @param string $group
+     * @param bool $exception
+     * @return array
      */
-    public static function delete(string $name): Timer
+    public static function get(string $group, bool $exception = true): array
     {
-        if (!array_key_exists($name, static::$timers)) {
-            throw new OutOfBoundsException('The specified timer ":name" does not exist', [':name' => $name]);
+        if (array_key_exists($group, static::$timers)) {
+            // Return the timers array
+            return static::$timers[$group];
         }
 
-        // Find the timer and remove it from the timers list, then return it
-        $timer = static::$timers[$name];
-        unset(static::$timers[$name]);
+        if ($exception) {
+            throw new TimerException(tr('Timers for the group ":group" do not exist', [
+                ':group' => $group,
+            ]));
+        }
 
+        return [];
+    }
+
+
+    /**
+     * Returns all the timers for the specified group and removes them all from the Timers object
+     *
+     * @param string $group
+     * @param bool $exception
+     * @return array
+     */
+    public static function pop(string $group, bool $exception = true): array
+    {
+        $timer = static::get($group, $exception);
+
+        unset(static::$timers[$group]);
         return $timer;
+    }
+
+
+    /**
+     * Returns the specified timer group
+     *
+     * @param string $group
+     * @return array
+     */
+    public static function delete(string $group): array
+    {
+        $timers = static::get($group);
+
+        // Remove the timer from the timers list, then return it
+        unset(static::$timers[$group]);
+        return $timers;
+    }
+
+
+    /**
+     * Returns all internal timers
+     *
+     * @return array
+     */
+    public static function getAll(): array
+    {
+        return static::$timers;
+    }
+
+
+    /**
+     * Returns the total for all the timer with the specified group
+     *
+     * @param string $group
+     * @return float
+     */
+    public static function getGroupTotal(string $group): float
+    {
+        $total = 0;
+
+        if (array_key_exists($group, static::$timers)) {
+            foreach (static::$timers[$group] as $timer) {
+                $total += $timer->getTotal();
+            }
+
+            return $total;
+        }
+
+        throw new TimerException(tr('Cannot return total for timer group ":group", the group does not exist', [
+            ':group' => $group
+        ]));
+    }
+
+
+    /**
+     * Returns the total for all the timers
+     *
+     * @return float
+     */
+    public static function getTotal(): float
+    {
+        $total = 0;
+
+        foreach (static::$timers as $group) {
+            foreach ($group as $timer) {
+                $total += $timer->getTotal();
+            }
+        }
+
+        return $total;
+    }
+
+
+    /**
+     * Ensures that the specified timers group exists
+     *
+     * @param string $group
+     * @return void
+     */
+    protected static function ensureGroup(string $group): void
+    {
+        if (!array_key_exists($group, static::$timers)) {
+            static::$timers[$group] = [];
+        }
     }
 }
