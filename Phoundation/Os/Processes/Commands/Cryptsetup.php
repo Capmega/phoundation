f21af3c1eb6f772c1b0e0a09e6e36d83c14e9026
Sven Olaf Oostenbrink <sven@medinet.ca>
2023-10-19 15:27:53 -0700

    Improved Processes Command Cryptsetup class
    Fixed Cryptsetup::luksFormat() Fixed various minor bugs, added logging
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Os/Processes/Commands/Cryptsetup.php
+++ Phoundation/Os/Processes/Commands/Cryptsetup.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Phoundation\Os\Processes\Commands;
 
 use Phoundation\Core\Config;
+use Phoundation\Core\Log\Log;
 use Phoundation\Core\Strings;
 use Phoundation\Exception\OutOfBoundsException;
 use Phoundation\Os\Devices\Storage\Device;
@@ -30,19 +31,23 @@ class Cryptsetup extends Command
      *
      * @param Device|string $device
      * @param string|null $key
-     * @param string|null $file_key
+     * @param string|null $key_file
      * @return void
      */
-    public function luksFormat(Device|string $device, string $key = null, string $file_key = null): void
+    public function luksFormat(Device|string $device, string $key = null, string $key_file = null): void
     {
         $device = Device::new($device)->getFile();
 
         if ($key) {
-            if ($file_key) {
+            if ($key_file) {
                 throw new OutOfBoundsException(tr('Cannot luks format device ":device", both key and file key specified', [
                     ':device' => $device
                 ]));
             }
+
+            Log::action(tr('Formatting device ":device" with LUKS encryption, this may take a few seconds', [
+                ':device' => $device
+            ]));
 
             Process::new('echo')
                 ->addArgument($key)
@@ -50,10 +55,10 @@ class Cryptsetup extends Command
                     ->setInternalCommand('cryptsetup')
                     ->setSudo(true)
                     ->addArguments(['-q', '-v', 'luksFormat', $device])
-                    ->setTimeout(10))
+                    ->setTimeout(30))
                 ->executePassthru();
         } else {
-            if (!$file_key) {
+            if (!$key_file) {
                 throw new OutOfBoundsException(tr('Cannot luks format device ":device", no key or file key specified', [
                     ':device' => $device
                 ]));
@@ -62,7 +67,7 @@ class Cryptsetup extends Command
             $this
                 ->setInternalCommand('cryptsetup')
                 ->setSudo(true)
-                ->addArguments(['-q', '-v', 'luksFormat', $device, '--key-file', $file_key])
+                ->addArguments(['-q', '-v', 'luksFormat', $device, '--key-file', $key_file])
                 ->setTimeout(10)
                 ->executePassthru();
         }
