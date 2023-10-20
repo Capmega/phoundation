5eaab0e638c3b120334ec752083640454fed9eba
Sven Olaf Oostenbrink <sven@medinet.ca>
2023-10-20 11:42:55 -0700

    Fixed OutOfBoundsExceptionInterface
    Fixed missing ExceptionInterface extend
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Exception/Interfaces/OutOfBoundsExceptionInterface.php
+++ Phoundation/Exception/Interfaces/OutOfBoundsExceptionInterface.php
@@ -7,10 +7,10 @@ namespace Phoundation\Exception\Interfaces;
  * Class OutOfBoundsException
  *
  * This is the phoundation version of the PHP OutOfBoundsException class, thrown whenever a variable or data is found
- * to be beyound acceptable limits
+ * to be beyond acceptable limits
  *
  * @package Phoundation\Exception
  */
-interface OutOfBoundsExceptionInterface
+interface OutOfBoundsExceptionInterface extends ExceptionInterface
 {
 }
