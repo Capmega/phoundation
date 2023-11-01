0617a452f848129bb5f3ef0bc3b3547ebfd2c43b
Sven Olaf Oostenbrink <sven@medinet.ca>
2023-11-01 11:52:23 -0700

    Fixed Data PostValidator class
    Fixed PostValidator::getSubmitButton() now correctly returns requested submit buttons, even if multiple, different buttons were specified
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Data/Validator/PostValidator.php
+++ Phoundation/Data/Validator/PostValidator.php
@@ -36,9 +36,9 @@ class PostValidator extends Validator
     /**
      * Tracks what button was pressed for the POST request
      *
-     * @var string|false|null $button
+     * @var array $buttons
      */
-    protected static string|false|null $button = null;
+    protected static array $buttons = [];
 
     /**
      * Tracks prefix for the pressed button
@@ -206,81 +206,137 @@ class PostValidator extends Validator
 
 
     /**
-     * Return the submit method
+     * Returns the requested button from the specified source
      *
-     * @param string $submit
+     * @param array $source
+     * @param string $post_key
      * @param bool $prefix
-     * @param bool $return_key
-     * @return string|false|null
+     * @param bool $remove
+     * @return array|null
      */
-    public static function getSubmitButton(string $submit = 'submit', bool $prefix = false, bool $return_key = false): string|false|null
+    protected static function getButton(array &$source, string $post_key, bool $prefix, bool $remove): array|null
     {
-        if (!Page::isPostRequestMethod()) {
-            return null;
-        }
-
-        // Return cache
-        if ($return_key and static::$key) {
-            return static::$key;
-
-        }
-
-        if (static::$button !== null) {
-            return static::$button;
-        }
-
-        // Search for the specified prefix code for the button
         if ($prefix) {
-            $prefix = $submit;
-            $button = null;
-
-            foreach (static::$post as $key => $value) {
-                if (str_ends_with($key, $submit)) {
-                    $submit = $key;
+            // Search for the specified prefix code for the button
+            foreach ($source as $key => $value) {
+                if (str_starts_with($key, $post_key)) {
+                    $post_key = $key;
                     $button = trim((string) $value);
                     break;
                 }
             }
 
+            if (!isset($button)) {
+                // No button with specified prefix found
+                return null;
+            }
+
         } else {
             // Button must be an exact match
-            $button = trim((string) isset_get(static::$post[$submit]));
+            if (!array_key_exists($post_key, $source)) {
+                // Button was not pressed
+                return null;
+            }
+
+            $button = trim((string) $source[$post_key]);
         }
 
-        if (!$submit) {
-            // Specified button not found
+        if ($remove) {
+            unset($source[$post_key]);
+        }
+
+        return [$post_key => $button];
+    }
+
+
+    /**
+     * Returns the value for the specified submit button for POST requests
+     *
+     * This method will search for and -if found- return the value for the specified HTTP POST key. By default it will
+     * search for the POST name "submit". If can scan in prefix mode, where it will search for HTTP POST keys that start
+     * with the specified POST key. If it finds a matching entry, that first entry will be returned.
+     *
+     * This method will not (and cannot) return if ANY button was pressed as it cannot see the difference between a
+     * button and a form value. A specific button name must be specified.
+     *
+     * @note This method will return NULL in case the requested submit button was not pressed
+     * @note For non POST requests this method will always return NULL
+     * @note For buttons that have an empty value (null, ""), this method will return TRUE instead
+     * @note The specified POST key, if found, will be removed from the POST array. If a prefix scan was requested, the
+     *       found (and returned) POST key will be removed from the POST array.
+     * @note The button values will be removed from the POST array but kept in cache, so calling this method twice for
+     *       the same button will return the same value, even though it was removed from POST after the first call.
+     * @note If $return_key AND $prefix were specified, this method will return the POST key FROM the specified
+     *       $post_key. So if $post_key was "delete_" and the found key was "delete_2342897342", this method will return
+     *       the value "2342897342" instead of "delete_2342897342"
+     *
+     * @param string $post_key
+     * @param bool $prefix      Will not return the specified POST $post_key value but scan for a POST key that starts
+     *                          with $post_key, and return that value.
+     * @param bool $return_key  If true, will return the found POST_KEY instead of the value.
+     * @return string|true|null
+     */
+    public static function getSubmitButton(string $post_key = 'submit', bool $prefix = false, bool $return_key = false): string|true|null
+    {
+        if (!Page::isPostRequestMethod()) {
             return null;
         }
 
-        unset(static::$post[$submit]);
+        // Return button from cache if available
+        $button = static::getButton(static::$buttons, $post_key, $prefix, false);
 
         if ($button) {
-            if ((strlen($button) > 255) or !ctype_print($button)) {
-                throw ValidationFailedException::new(tr('Invalid submit button specified'))->addData([
-                    'submit' => tr('The specified submit button is invalid'),
-                ]);
+            // We had it from cache. Get button key and value
+            $key   = (string) key($button);
+            $value = (string) current($button);
+
+        } else {
+            // Not cached. Get button from post and remove it there, then store it in cache
+            $button = static::getButton(static::$post, $post_key, $prefix, true);
+
+            if (!$button) {
+                // Button was not in cache nor in POST
+                return null;
             }
+
+            // Get button key and value
+            $key   = (string) key($button);
+            $value = (string) current($button);
+
+            // Quick validate button value. Don't allow weird shit
+            if ($key) {
+                if ((strlen($key) > 255) or !ctype_print($key)) {
+                    throw ValidationFailedException::new(tr('Invalid submit button specified'))->addData([
+                        'submit' => tr('The specified submit button is invalid'),
+                    ]);
+                }
+            }
+
+            if ($value) {
+                if ((strlen($value) > 255) or !ctype_print($value)) {
+                    throw ValidationFailedException::new(tr('Invalid submit button specified'))->addData([
+                        'submit' => tr('The specified submit button is invalid'),
+                    ]);
+                }
+            }
+
+            static::$buttons[$key] = $value;
         }
 
+        // We have a button, yay!
         if ($return_key) {
-            static::$key = Strings::until($submit, $prefix);
-
-            if ((strlen(static::$key) > 32) or !ctype_print(static::$key)) {
-                throw ValidationFailedException::new(tr('Invalid submit button specified'))->addData([
-                    'submit' => tr('The specified submit button is invalid'),
-                ]);
+            if ($prefix) {
+                return Strings::from($key, $post_key);
             }
 
-            return Strings::until($submit, $prefix);
+            return $key;
         }
 
-        static::$button = $button;
-
-        if (!$button) {
+        if (!$value) {
             // Button exists, but has no value
-            return false;
+            return true;
         }
 
-        return $button;
+        return $value;
     }
 }
