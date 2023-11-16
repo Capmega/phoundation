d6df2e6b2742fea018679e88b7a91405bd04bb27
Sven Olaf Oostenbrink <sven@medinet.ca>
2023-11-15 16:43:48 -0800

    Improved Cli class
    Improved Cli::displayTable() can now display tables without ID column
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Cli/Cli.php
+++ Phoundation/Cli/Cli.php
@@ -127,13 +127,13 @@ class Cli
         if ($source) {
             // Get headers from columns
             if ($headers === null) {
-                $value   = str_replace(['_', '-'], ' ', $id_column);
+                $value   = str_replace(['_', '-'], ' ', (string) $id_column);
                 $value   = Strings::capitalize($value) . ':';
                 $headers = ($id_column ? [$id_column => $value] : []);
                 $row     = current($source);
 
-                foreach ($row as $header => $value) {
-                    $value = str_replace(['_', '-'], ' ', $header);
+                foreach (Arrays::force($row, null) as $header => $value) {
+                    $value = str_replace(['_', '-'], ' ', (string) $header);
                     $value = Strings::capitalize($value) . ':';
 
                     $headers[$header] = $value;
@@ -141,32 +141,33 @@ class Cli
             }
 
             // Display header
-            foreach ($headers as $column => $header) {
-                $column_sizes[$column] = Numbers::getHighest($column_sizes[$column], strlen($header));
-                Log::cli(Color::apply(Strings::size((string) $header, $column_sizes[$column]), 'white') . Strings::size(' ', $column_spacing), 10, false);
-            }
+            if (!VERY_QUIET) {
+                foreach ($headers as $column => $header) {
+                    $column_sizes[$column] = Numbers::getHighest($column_sizes[$column], strlen($header));
+                    Log::cli(Color::apply(Strings::size((string) $header, $column_sizes[$column]), 'white') . Strings::size(' ', $column_spacing), 10, false);
+                }
 
-            Log::cli();
+                Log::cli();
+            }
 
             // Display source
             foreach ($source as $id => $row) {
-                if (!is_array($row)) {
-                    // Wrong! This is a row and as such should be an array
-                    throw new OutOfBoundsException(tr('Invalid row ":row" specified for id ":id", it should be an array', [
-                        ':id' => $id,
-                        ':row' => $row,
-                    ]));
+                $row = Arrays::force($row, null);
+
+                if ($id_column) {
+                    array_unshift($row, $id);
                 }
 
-                array_unshift($row, $id);
-
+                // Display all row cells
                 foreach ($row as $column => $value) {
                     if ($column === 0) {
                         // Due to the nature of array_unshift (we can't specify key name, so it always has key 0), rename!
-                        $column = $id_column;
+                        if ($id_column) {
+                            $column = $id_column;
+                        }
                     }
 
-                    if (array_key_exists($column, $headers)) {
+                    if (is_numeric($column) or array_key_exists($column, $headers)) {
                         Log::cli(Strings::size((string) $value, $column_sizes[$column], ' ', is_numeric($value)) . Strings::size(' ', $column_spacing), 10, false);
                     }
                 }
@@ -174,7 +175,7 @@ class Cli
                 Log::cli();
             }
         } else {
-            // Oops, there are no results!
+            // Oops, empty source!
             Log::warning(tr('No results'));
         }
     }
@@ -207,7 +208,7 @@ class Cli
         }
 
         // Determine the size of the keys to display them
-        $key_size = Arrays::getLongestKeySize($source) + 4;
+        $key_size = Arrays::getLongestKeyLength($source) + 4;
 
         // Display header
         if ($key_header and $value_header) {
