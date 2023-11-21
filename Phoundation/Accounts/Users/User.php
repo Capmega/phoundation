788de5389bd785bbe215d0a61ea2cc0294bdda38
Sven Olaf Oostenbrink <sven@medinet.ca>
2023-11-20 16:38:08 -0800

    Fixed Accounts User class
    Fixed User::get() interface compatibility
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Accounts/Users/User.php
+++ Phoundation/Accounts/Users/User.php
@@ -225,9 +225,10 @@ class User extends DataEntry implements UserInterface
      *
      * @param DataEntryInterface|string|int|null $identifier
      * @param string|null $column
+     * @param bool $meta_enabled
      * @return User|null
      */
-    public static function get(DataEntryInterface|string|int|null $identifier = null, ?string $column = null): ?static
+    public static function get(DataEntryInterface|string|int|null $identifier = null, ?string $column = null, bool $meta_enabled = true): ?static
     {
         try {
             return parent::get($identifier, $column);
