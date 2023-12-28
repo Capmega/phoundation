0b95d98a9a924dfdb604f7422c2a043ae9327421
Sven Olaf Oostenbrink <sven@medinet.ca>
2023-12-27 16:40:38 -0800

    Improved Utils Json class
    Added Json::ensureDecoded(), Json::ensureEncoded() will ensure that the given source is as requested
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Utils/Json.php
+++ Phoundation/Utils/Json.php
@@ -401,6 +401,28 @@ class Json
 
 
     /**
+     * Ensure that the specified source is encoded into a JSON string
+     *
+     * This method will assume that given strings are encoded JSON. Anything else will be encoded into a JSON string
+     *
+     * @param mixed $source
+     * @param int $options
+     * @param int $depth Until what depth will we recurse until an exception will be thrown
+     * @return string
+     * @throws JsonException If JSON encoding failed
+     */
+    public static function ensureEncoded(mixed $source, int $options = 0, int $depth = 512): string
+    {
+        if (is_string($source)) {
+            // Assume this is a JSON string
+            return $source;
+        }
+
+        return static::encode($source, $options, $depth);
+    }
+
+
+    /**
      * Decode the given JSON string back into the original data. Can optionally decode into standard object classes or
      * arrays [default]
      *
@@ -429,12 +451,39 @@ class Json
 
 
     /**
+     * Ensure the given variable is decoded.
+     *
+     * If it is a JSON string it will be decoded back into the original data. If it is not a string, this method will
+     * assume it already was decoded. Can optionally decode into standard object classes or arrays [default]
+     *
+     * @param mixed $source
+     * @param int $options
+     * @param int $depth Until what depth will we recurse until an exception will be thrown
+     * @param bool $as_array If $as_array is set true [default] then this method will always return an array. If not,
+     *                       it will return a PHP JSON object
+     * @return mixed The decoded variable
+     * @throws JsonException
+     */
+    public static function ensureDecoded(mixed $source, int $options = 0, int $depth = 512, bool $as_array = true): mixed
+    {
+        if ($source and is_string($source)) {
+            return static::decode($source, $options, $depth, $as_array);
+        }
+
+        return $source;
+    }
+
+
+    /**
      * Returns the specified source, but limiting its maximum size to the specified $max_size.
      *
      * If it crosses this threshold, it will truncate entries in the $source array
      *
      * @param array|string $source
      * @param int $max_size
+     * @param string $fill
+     * @param string $method
+     * @param bool $on_word
      * @param int $options
      * @param int $depth
      * @return string
