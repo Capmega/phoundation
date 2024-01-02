ef05bb56db9cf67747d63017c008d5aa1805fb0b
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-01-02 11:53:19 -0800

    Cleaned code
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Servers/Server.php
+++ Phoundation/Servers/Server.php
@@ -40,7 +40,7 @@ use Phoundation\Web\Html\Enums\InputTypeExtended;
  *
  * This class manages a single server
  *
- * @see \Phoundation\Data\DataEntry\DataEntry
+ * @see DataEntry
  * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
  * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
  * @copyright Copyright (c) 2023 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
