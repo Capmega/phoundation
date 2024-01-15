5dc9d780a96691efde38aeca24cfc006403cfc5f
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-01-15 09:50:43 -0800

    Cleaned code
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Utils/Config.php
+++ Phoundation/Utils/Config.php
@@ -31,7 +31,7 @@ use Throwable;
  * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
  * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
  * @copyright Copyright (c) 2023 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
- * @package Phoundation\Core
+ * @package Phoundation\Utils
  */
 class Config implements ConfigInterface
 {
