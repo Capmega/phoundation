3aedf0e8864cb806fc0e234be0730ac9274e6879
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2024-01-21 18:49:15 -0800

    Improved Os Commands ScanImage class
    Implemented ScanImage::listDevices()
    
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

--- Phoundation/Os/Processes/Commands/ScanImage.php
+++ Phoundation/Os/Processes/Commands/ScanImage.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Phoundation\Os\Processes\Commands;
 
+use Phoundation\Core\Log\Log;
 use Phoundation\Os\Processes\Enum\EnumExecuteMethod;
 use Phoundation\Os\Processes\Enum\Interfaces\EnumExecuteMethodInterface;
+use Phoundation\Utils\Strings;
 
 
 /**
@@ -28,11 +30,33 @@ class ScanImage extends Command
      */
     public function listDevices(EnumExecuteMethodInterface $method = EnumExecuteMethod::noReturn): string|int|bool|array|null
     {
+        $output = $this
+            ->setCommand('scanimage')
+            ->addArguments(['--formatted-device-list', '%d^^^%v^^^%m^^^%t^^^%i'])
+            ->setTimeout(60)
+            ->executeReturnArray();
+
+        // Parse the output
+        $return = [];
+
+        foreach ($output as $line_number => $line) {
+            $line = explode('^^^', $line);
+
+            $return[$line[0]] = [
+                'device' => $line[0],
+                'vendor' => $line[1],
+                'model'  => $line[2],
+                'class'  => $line[3],
+                'index'  => $line[4],
+            ];
+        }
+
+        return $return;
     }
 
 
     /**
-     *
+     * Execute the configured scan
      *
      * @param EnumExecuteMethodInterface $method
      * @return string|int|bool|array|null
