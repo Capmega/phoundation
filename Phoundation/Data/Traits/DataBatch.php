dc81870518cf01a04d628e90274f5fd1f3af88ec
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-01-25 10:21:36 -0800

    Added more Traits
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

---
+++ Phoundation/Data/Traits/DataBatch.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phoundation\Data\Traits;
+
+
+/**
+ * Trait DataBatch
+ *
+ *
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://openbatch.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Data
+ */
+trait DataBatch
+{
+    protected bool $batch = false;
+
+
+    /**
+     * Returns if the scan job is a batch
+     *
+     * @param bool $batch
+     * @return bool
+     */
+    public function getBatch(bool $batch): bool
+    {
+        return $this->batch;
+    }
+
+
+    /**
+     * Sets if the scan job is a batch
+     *
+     * @param bool $batch
+     * @return $this
+     */
+    public function setBatch(bool $batch): static
+    {
+        $this->batch = $batch;
+        return $this;
+    }
+}
