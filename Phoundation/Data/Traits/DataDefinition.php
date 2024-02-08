0fe330b991cac90b98acc073a1d382a65e12f284
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-02-08 14:35:44 -0800

    Added more traits
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

---
+++ Phoundation/Data/Traits/DataDefinition.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phoundation\Data\Traits;
+
+use Phoundation\Data\DataEntry\Definitions\Interfaces\DefinitionInterface;
+
+
+/**
+ * Trait DataDefinition
+ *
+ *
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Data
+ */
+trait DataDefinition
+{
+    /**
+     * The definition to use
+     *
+     * @var DefinitionInterface|null $definition
+     */
+    protected ?DefinitionInterface $definition = null;
+
+
+    /**
+     * Returns the definition
+     *
+     * @return DefinitionInterface|null
+     */
+    public function getDefinition(): ?DefinitionInterface
+    {
+        return $this->definition;
+    }
+
+
+    /**
+     * Sets the definition
+     *
+     * @param DefinitionInterface|null $definition
+     * @return static
+     */
+    public function setDefinition(DefinitionInterface|null $definition): static
+    {
+        $this->definition = $definition;
+        return $this;
+    }
+}
