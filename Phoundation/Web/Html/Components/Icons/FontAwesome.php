328e5f214c92cb86cca7f55cd1947a1a47ae6afe
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-03-05 15:15:17 -0800

    Improved Web Components Icons FontAwesome class
    Updated to work with the new icon names
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Web/Html/Components/Icons/FontAwesome.php
+++ Phoundation/Web/Html/Components/Icons/FontAwesome.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Phoundation\Web\Html\Components\Icons;
 
-use Phoundation\Utils\Strings;
-
 
 /**
  * FontAwesome class
@@ -27,18 +25,6 @@ class FontAwesome extends Icon
     public function __construct(?string $content = null)
     {
         parent::__construct($content);
-        $this->addClasses(['far']);
-    }
-
-
-    /**
-     * Returns the icon for this object
-     *
-     * @return string|null
-     */
-    public function getIcon(): ?string
-    {
-        return Strings::startsNotWith($this->icon, 'fa-');
     }
 
 
@@ -51,8 +37,6 @@ class FontAwesome extends Icon
      */
     public function setIcon(?string $icon, string $subclass = 'far fas fab'): static
     {
-        $icon = strtolower(trim($icon));
-
-        return parent::setIcon('fa-' . $icon, $subclass);
+        return parent::setIcon(strtolower(trim($icon)), $subclass);
     }
 }
