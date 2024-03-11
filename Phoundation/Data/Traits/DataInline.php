a65044eb1a9cd49f23443e8384c1d0863ee53da7
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-03-11 15:28:10 -0700

    Added new trait
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

---
+++ Phoundation/Data/Traits/DataInline.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phoundation\Data\Traits;
+
+
+/**
+ * Trait DataInline
+ *
+ *
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opendebug.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Data
+ */
+trait DataInline
+{
+    /**
+     * Tracks if the meta-system is enabled or disabled for this (type of) DataEntry
+     *
+     * @var bool $inline
+     */
+    protected bool $inline = true;
+
+
+    /**
+     * Returns if the meta-system is enabled or disabled for this (type of) DataEntry
+     *
+     * @return bool
+     */
+    public function getInline(): bool
+    {
+        return $this->inline;
+    }
+
+
+    /**
+     * Sets if the meta-system is enabled or disabled for this (type of) DataEntry
+     *
+     * @param bool $inline
+     * return static
+     */
+    public function setInline(bool $inline): static
+    {
+        $this->inline = $inline;
+        return $this;
+    }
+}
