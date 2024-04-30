a9708a6b72183ceead4b6b2ad86e4eb512d0764c
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-04-30 15:17:06 -0700

    Added git auto commit functionality
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Cli/Library/commands/commands/cache/rebuild.php
+++ Phoundation/Cli/Library/commands/commands/cache/rebuild.php
@@ -1,24 +1,63 @@
 <?php
 
-declare(strict_types=1);
-
-use Phoundation\Core\Libraries\Libraries;
-use Phoundation\Data\Validator\ArgvValidator;
-
-
 /**
- * Script commands/cache/rebuild
+ * Command commands cache rebuild
  *
  * This command will rebuild the commands cache
  *
  * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
  * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
  * @copyright Copyright (c) 2022 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
- * @package   Phoundation\Scripts
+ * @package   Phoundation\Cli
  */
 
+declare(strict_types=1);
 
-ArgvValidator::new()->validate();
+use Phoundation\Cli\CliDocumentation;
+use Phoundation\Core\Libraries\Libraries;
+use Phoundation\Data\Validator\ArgvValidator;
+use Phoundation\Developer\Versioning\Git\Git;
+use Phoundation\Utils\Config;
+
+CliDocumentation::setUsage('./pho commands cache rebuild [OPTIONS]
+./pho rebuild
+./pho rebuild --commit
+./pho rebuild --sign
+');
+
+CliDocumentation::setHelp('This command will rebuild all the system caches and automatically commit the updated
+cache to git
 
 
+ARGUMENTS
+
+
+-c,--commit                             If specified will commit the update to git, even if configured not to
+
+-s,--sign                               If specified will sign the commit, even if configured not to
+');
+
+CliDocumentation::setAutoComplete([
+                                      'arguments' => [
+                                          '-s,--sign'   => false,
+                                          '-c,--commit' => false,
+                                      ],
+                                  ]);
+
+
+// Get command arguments
+ArgvValidator::new()
+             ->select('-c,--commit')->isOptional(false)->isBoolean()
+             ->select('-s,--sign')->isOptional(false)->isBoolean()
+             ->validate();
+
+
+// Rebuild commands cache
 Libraries::rebuildCommandCache();
+
+
+if (Config::getBoolean('versioning.git.commit.auto', false) or $argv['commit']) {
+    // Commit the system commands cache
+    Git::new(DIRECTORY_DATA . 'system/cache/commands/')
+       ->commit('Rebuilt system commands cache', Config::getBoolean('versioning.git.commit.signed', false) or $argv['signed']);
+}
--- Phoundation/Web/Library/commands/web/cache/rebuild.php
+++ Phoundation/Web/Library/commands/web/cache/rebuild.php
@@ -1,13 +1,7 @@
 <?php
 
-declare(strict_types=1);
-
-use Phoundation\Data\Validator\ArgvValidator;
-use Phoundation\Web\Web;
-
-
 /**
- * Script web pages web/cache/rebuild
+ * Command web cache rebuild
  *
  * This command rebuilds the web cache
  *
@@ -17,10 +11,53 @@ use Phoundation\Web\Web;
  * @package   Phoundation\Web
  */
 
+declare(strict_types=1);
 
-// This command accepts no arguments
-ArgvValidator::new()->validate();
+use Phoundation\Cli\CliDocumentation;
+use Phoundation\Data\Validator\ArgvValidator;
+use Phoundation\Developer\Versioning\Git\Git;
+use Phoundation\Utils\Config;
+use Phoundation\Web\Web;
+
+CliDocumentation::setUsage('./pho web cache rebuild [OPTIONS]
+./pho rebuild
+./pho rebuild --commit
+./pho rebuild --sign
+');
+
+CliDocumentation::setHelp('This command will rebuild all the system caches and automatically commit the updated
+cache to git
+
+
+ARGUMENTS
+
+
+-c,--commit                             If specified will commit the update to git, even if configured not to
+
+-s,--sign                               If specified will sign the commit, even if configured not to
+');
+
+CliDocumentation::setAutoComplete([
+                                      'arguments' => [
+                                          '-s,--sign'   => false,
+                                          '-c,--commit' => false,
+                                      ],
+                                  ]);
+
+
+// Get command arguments
+ArgvValidator::new()
+             ->select('-c,--commit')->isOptional(false)->isBoolean()
+             ->select('-s,--sign')->isOptional(false)->isBoolean()
+             ->validate();
 
 
 // Rebuild web cache
 Web::rebuildCache();
+
+
+if (Config::getBoolean('versioning.git.commit.auto', false) or $argv['commit']) {
+    // Commit the system web cache
+    Git::new(DIRECTORY_DATA . 'system/cache/web/')
+       ->commit('Rebuilt system web cache', Config::getBoolean('versioning.git.commit.signed', false) or $argv['signed']);
+}
