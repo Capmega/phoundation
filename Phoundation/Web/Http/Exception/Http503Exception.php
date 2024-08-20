c62c0f65e489ca6580a1d4af62b38edff36dc357
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-08-20 15:56:44 -0700

    Added Http503Exception
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

---
+++ Phoundation/Web/Http/Exception/Http503Exception.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Class Http503Exception
+ *
+ * This is the standard HTTP 503 - Server maintenance
+ *
+ * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package   Phoundation\Web
+ */
+
+
+declare(strict_types=1);
+
+namespace Phoundation\Web\Http\Exception;
+
+class Http503Exception extends HttpException
+{
+}
