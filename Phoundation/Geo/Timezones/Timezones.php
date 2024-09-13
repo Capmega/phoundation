1ff23448e969b2fbdb10aeb7c34e1899a77b76dc
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2024-09-13 09:08:04 -0700

    Updated Geo library
    Updated Phoundation calls
    
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

--- Phoundation/Geo/Timezones/Timezones.php
+++ Phoundation/Geo/Timezones/Timezones.php
@@ -99,9 +99,9 @@ class Timezones extends DataIterator
      *
      * @return HtmlTableInterface
      */
-    public function getHtmlTable(array|string|null $columns = null): HtmlTableInterface
+    public function getHtmlTableObject(array|string|null $columns = null): HtmlTableInterface
     {
-        $table = parent::getHtmlTable();
+        $table = parent::getHtmlTableObject();
         $table->setCheckboxSelectors(EnumTableIdColumn::checkbox);
 
         return $table;
