915f80b0ba425d52ab6bb19362652f9a6ced4196
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-10-02 15:24:55 -0700

    Improved Web library
    Improved InputInterface, now also extends BeforeAfterButtonsInterface containing input buttons
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Web/Html/Components/Input/Interfaces/BeforeAfterButtonsInterface.php
+++ Phoundation/Web/Html/Components/Input/Interfaces/BeforeAfterButtonsInterface.php
@@ -4,84 +4,12 @@ declare(strict_types=1);
 
 namespace Phoundation\Web\Html\Components\Input\Interfaces;
 
-use Phoundation\Data\DataEntry\Definitions\Interfaces\DefinitionInterface;
-use Phoundation\Web\Html\Components\Icons\Interfaces\IconInterface;
 use Phoundation\Web\Html\Components\Input\Buttons\Interfaces\ButtonInterface;
 use Phoundation\Web\Html\Components\Input\Buttons\Interfaces\ButtonsInterface;
-use Phoundation\Web\Html\Components\Interfaces\ElementInterface;
 
-interface InputInterface extends ElementInterface, BeforeAfterButtonsInterface
+
+interface BeforeAfterButtonsInterface
 {
-    /**
-     * Input class constructor
-     */
-    function __construct();
-
-
-    /**
-     * Returns a new input element from
-     *
-     * @param DefinitionInterface $definition
-     *
-     * @return static
-     */
-    public static function newFromDataEntryDefinition(DefinitionInterface $definition): static;
-
-
-    /**
-     * Returns the description
-     *
-     * @return string|null
-     */
-    public function getDescription(): ?string;
-
-
-    /**
-     * Sets the description
-     *
-     * @param string|null $description
-     * @param bool        $make_safe
-     *
-     * @return static
-     */
-    public function setDescription(?string $description, bool $make_safe = true): static;
-
-
-    /**
-     * Returns the icon
-     *
-     * @return IconInterface|null
-     */
-    public function getIcon(): ?IconInterface;
-
-
-    /**
-     * Sets the icon
-     *
-     * @param IconInterface|null $icon
-     *
-     * @return static
-     */
-    public function setIcon(?IconInterface $icon): static;
-
-
-    /**
-     * Returns if the input element has a clear button or not
-     *
-     * @return bool
-     */
-    public function getClearButton(): bool;
-
-
-    /**
-     * Sets if the input element has a clear button or not
-     *
-     * @param bool $clear_button
-     *
-     * @return static
-     */
-    public function setClearButton(bool $clear_button): static;
-
     /**
      * Returns if this input element has after buttons
      *
