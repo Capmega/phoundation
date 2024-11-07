8ccd386f7db5d79dea904fae08e77559e1506b80
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-11-06 16:12:11 -0800

    Improved Notifications library
    Details column can now be null
    
    Notification can no longer be changed status (nor be made "read") by impersonating users to avoid account impersonators trying to hide evidence
    
    Cleaned code
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Notifications/Library/web/pages/notifications/test.php
+++ Phoundation/Notifications/Library/web/pages/notifications/test.php
@@ -24,14 +24,14 @@ use Phoundation\Web\Requests\Response;
 
 // Create the notification, log it, and send it to this user.
 Notification::new()
-    ->setUrl('/index.html')
-    ->setMode(pick_random_argument(EnumDisplayMode::error, EnumDisplayMode::warning, EnumDisplayMode::success, EnumDisplayMode::info, EnumDisplayMode::notice))
-    ->setUsersId(Session::getUserObject()->getId())
-    ->setTitle(tr('This is a test notification'))
-    ->setMessage(tr('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'))
-    ->setDetails(['test' => Strings::getRandom(16)])
-    ->log()
-    ->send();
+            ->setUrl(Url::getWww('/index.html'))
+            ->setMode(pick_random_argument(EnumDisplayMode::error, EnumDisplayMode::warning, EnumDisplayMode::success, EnumDisplayMode::info, EnumDisplayMode::notice))
+            ->setUsersId(Session::getUserObject()->getId())
+            ->setTitle(tr('This is a test notification'))
+            ->setMessage(tr('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'))
+            ->setDetails(['test' => Strings::getRandom(16)])
+            ->log()
+            ->send();
 
-// Redirect to the all notifications page
+// Redirect to the "all notifications" page
 Response::redirect(Url::getPrevious('/notifications/notifications.html'));
