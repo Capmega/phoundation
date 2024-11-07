dbb3ddbf2f6035f3ed25bbcde09e18d977b265bb
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-11-06 17:08:50 -0800

    Fixed Audio library
    Fixed play command
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Audio/Library/commands/audio/play.php
+++ Phoundation/Audio/Library/commands/audio/play.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * Command image convert
+ * Command audio play
  *
- * This script can apply various conversions to the specified image
+ * This command can play audio files on the local machine
  *
  * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
  * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
@@ -18,7 +18,17 @@ use Phoundation\Audio\Audio;
 use Phoundation\Cli\CliDocumentation;
 use Phoundation\Core\Log\Log;
 use Phoundation\Data\Validator\ArgvValidator;
+use Phoundation\Filesystem\PhoDirectory;
 
+
+CliDocumentation::setAutoComplete([
+    'positions' => [
+        0 => [
+            'word'   => function($word) { return PhoDirectory::newFilesystemRootObject(false)->scan($word, '/.*?\.mp3$/')->addEmpty(); },
+            'noword' => function($word) { return PhoDirectory::newFilesystemRootObject(false)->scan($word, '/.*?\.mp3$/')->addEmpty(); },
+        ]
+    ]
+]);
 
 CliDocumentation::setUsage('./pho audio play FILENAME
 ./pho system audio play PATH/FILE');
@@ -38,7 +48,7 @@ PATH/FILE                               The file to play directly
 
 
 $argv = ArgvValidator::new()
-                     ->select('file')->sanitizeFile()
+                     ->select('file')->sanitizeFile(PhoDirectory::newFilesystemRootObject())
                      ->select('-b,--background')->isOptional()->isBoolean()
                      ->validate();
 
