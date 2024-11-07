474da3312e620261eedb9387d391b7618a98e449
Harrison Macey <harrison@medinet.ca>
2024-11-07 09:00:59 -0800

    Improved Developer library
    Improved show command to correctly show JSON encoded strings
    
    Signed-off-by: Harrison Macey <harrison@medinet.ca>

--- Phoundation/Developer/Debug.php
+++ Phoundation/Developer/Debug.php
@@ -408,7 +408,7 @@ class Debug
                             ':file' => static::currentFile($trace_offset),
                             ':line' => static::currentLine($trace_offset),
                             ':size' => strlen((string) $value),
-                        ])) . Strings::log($value, ensure_visible: true) . PHP_EOL;
+                        ])) . Strings::log($value, ensure_visible: true, clean: false) . PHP_EOL;
 
                 } else {
                     // Sort if is array for easier reading
