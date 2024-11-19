f42e403e7fa39e982f23316e0e9ca0e1b83e9e93
Harrison Macey <harrison@medinet.ca>
2024-11-18 16:37:29 -0800

    Improved Filesystem library
    Added function newDataObject()
    
    Signed-off-by: Harrison Macey <harrison@medinet.ca>

--- Phoundation/Filesystem/PhoFile.php
+++ Phoundation/Filesystem/PhoFile.php
@@ -17,6 +17,7 @@ declare(strict_types=1);
 namespace Phoundation\Filesystem;
 
 use Phoundation\Filesystem\Interfaces\PhoFileInterface;
+use Phoundation\Filesystem\Interfaces\PhoRestrictionsInterface;
 use Phoundation\Filesystem\Traits\TraitPathConstructor;
 use Phoundation\Filesystem\Traits\TraitPathNew;
 use Phoundation\Utils\Strings;
@@ -26,6 +27,20 @@ class PhoFile extends PhoFileCore
 {
     use TraitPathConstructor;
     use TraitPathNew;
+
+
+    /**
+     * Returns a new file object for a file in data/...
+     *
+     * @param string                   $file
+     * @param PhoRestrictionsInterface $restrictions
+     *
+     * @return PhoFileInterface
+     */
+    public static function newDataObject(string $file, PhoRestrictionsInterface $restrictions): PhoFileInterface
+    {
+        return static::new(DIRECTORY_DATA . $file, $restrictions);
+    }
 
 
     /**
