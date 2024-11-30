47328ccb0f2150e6e6aed7a8d79d6f9df9361085
Harrison Macey <harrison@medinet.ca>
2024-11-29 16:23:00 -0800

    Improved Data library
    Added more test cases to Unit test class
    
    Updates `Updates` class to include new column needed for testing in TestDataEntry object
    
    Added test_column as property to TestDataEntry object
    
    Signed-off-by: Harrison Macey <harrison@medinet.ca>

--- Phoundation/Data/Library/Tests/Phoundation/Data/DataEntry/DataEntryTest.php
+++ Phoundation/Data/Library/Tests/Phoundation/Data/DataEntry/DataEntryTest.php
@@ -18,11 +18,12 @@ declare(strict_types=1);
 
 namespace Phoundation\Data\Library\Tests\Phoundation\Data\DataEntry;
 
-use Phoundation\Core\Sessions\Session;
 use Phoundation\Data\DataEntry\DataEntry;
+use Phoundation\Data\DataEntry\Exception\DataEntryException;
 use Phoundation\Data\DataEntry\Tests\TestDataEntry;
 use Phoundation\Utils\Numbers;
 use PHPUnit\Framework\TestCase;
+use Throwable;
 
 class DataEntryTest extends TestCase
 {
@@ -55,9 +56,12 @@ class DataEntryTest extends TestCase
      */
     public function testNewRandomId()
     {
-        $id = Numbers::getRandomInt();
+        $id    = Numbers::getRandomInt();
+        $entry = TestDataEntry::new($id);
 
-        $this->assertEquals($id, TestDataEntry::new($id)->getId());
+        $entry->save();
+
+        $this->assertEquals($id, $entry->getId(), 'The TestDataEntry object should have the ID it was initialized with');
     }
 
 
@@ -72,5 +76,126 @@ class DataEntryTest extends TestCase
         $expectedColumns = ['id', 'created_on', 'created_by', 'meta_id', 'status', 'meta_state'];
 
         $this->assertEquals($expectedColumns, $entry->getDefaultMetaColumns(), 'getDefaultMetaColumns should return default metadata columns');
+    }
+
+    /**
+     * Tests DataEntry::getIdColumn()
+     *
+     * @return void
+     */
+    public function testGetIdColumn()
+    {
+        $entry = TestDataEntry::new();
+
+        $this->assertEquals('id', $entry->getIdColumn(), 'getIdColumn should return the "id" column');
+    }
+
+
+    /**
+     * Tests DataEntry::isNew()
+     *
+     * @return void
+     */
+    public function testIsNew()
+    {
+        $entry = TestDataEntry::new();
+
+        $this->assertTrue($entry->isNew(), 'The TestDataEntry object that is not yet written to a database should be new');
+
+        $entry->setStatus('saved');
+
+        $this->assertFalse($entry->isNew(), 'The TestDataEntry object that has been written to a database should NOT be new');
+    }
+
+
+    /**
+     * Tests DataEntry::setUniqueColumnValue()
+     * Tests DataEntry::getUniqueColumnValue()
+     *
+     * @return void
+     */
+    public function testGetUniqueColumnValue()
+    {
+        $entry = TestDataEntry::new();
+
+        $this->assertNull($entry->getUniqueColumnValue(), 'The TestDataEntry object should have no Unique Column Value');
+
+        $value = 'unique-code';
+
+        $entry->setUniqueColumnValue($value);
+
+        $this->assertEquals($value, $entry->getUniqueColumnValue(), 'The TestDataEntry object should have the specified Unique Column Value');
+    }
+
+    /**
+     * Tests DataEntry::set()
+     *
+     * @return void
+     */
+    public function testSet()
+    {
+        $entry = TestDataEntry::new();
+
+        $test_value = 'test-value';
+
+        // Test setting to invalid column
+        try {
+            $entry->set($test_value, 'invalid-column');
+
+        } catch (Throwable $e) {
+            $this->AssertEquals(DataEntryException::class, $e::class, 'A DataEntryException should have been thrown');
+        }
+
+        // Test setting to ignored column // TODO: add ignored column
+
+        // Test setting successfully
+        $test_key   = 'test_column';
+        $entry->set($test_value, $test_key);
+
+        $this->assertEquals($test_value, $entry->get($test_key), 'The TestDataEntry object should have the specified value');
+    }
+
+
+    /**
+     * Tests DataEntry::get()
+     *
+     * @return void
+     */
+    public function testGet()
+    {
+        // TODO
+        $entry = TestDataEntry::new();
+    }
+
+
+    /**
+     * Tests DataEntry::getTypesafe()
+     *
+     * @return void
+     */
+    public function testGetMetaColumns()
+    {
+        $entry = TestDataEntry::new();
+
+        $meta_columns = ['id', 'created_on', 'created_by', 'meta_id', 'status', 'meta_state'];
+
+        $this->assertEquals($meta_columns, $entry->getMetaColumns(), 'getMetaColumns should return meta data columns');
+    }
+
+
+    /**
+    * Tests DataEntry::getSourceKeys()
+    *
+    * @return void
+    */
+    public function testGetSourceKeys()
+    {
+        $entry = TestDataEntry::new();
+
+        $source_keys_unfiltered = ['id', 'created_on', 'created_by', 'meta_id', 'status', 'meta_state', 'seo_name', 'name', 'test_column', 'parents_id', 'description'];
+        $source_keys_filtered   = ['seo_name', 'name', 'test_column', 'parents_id', 'description'];
+
+        $this->AssertEquals($source_keys_unfiltered, $entry->getSourceKeys(), 'getSourceKeys() should return the source keys, including meta columns');
+        $this->AssertEquals($source_keys_filtered, array_values($entry->getSourceKeys(true)), 'getSourceKeys() should return the filtered source keys, not including meta columns');
     }
 }
