6803aacef7567402e90502002b0759c6476e291f
Sven Olaf Oostenbrink <sven@medinet.ca>
2025-01-06 14:18:26 -0800

    Improved Data library
    Added TraitDataSkipIterator, TraitDataIgnoreIterator
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

---
+++ Phoundation/Data/Traits/TraitDataIgnoreIterator.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Trait TraitDataIgnoreIterator
+ *
+ * This trait contains the basic methods required to use an ignore iterator list
+ *
+ * The ignore list can be used to track a list of items that should be ignored
+ *
+ * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license   http://openignore.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package   Phoundation\Data
+ */
+
+
+declare(strict_types=1);
+
+namespace Phoundation\Data\Traits;
+
+use Phoundation\Data\Interfaces\IteratorInterface;
+use Phoundation\Data\Iterator;
+
+
+trait TraitDataIgnoreIterator
+{
+    /**
+     * The list of items to ignore
+     *
+     * @var IteratorInterface|null $ignore
+     */
+    protected ?IteratorInterface $ignore = null;
+
+
+    /**
+     * Returns the ignore list
+     *
+     * @param bool $force
+     *
+     * @return IteratorInterface|null
+     */
+    public function getIgnoreObject(bool $force = false): ?IteratorInterface
+    {
+        if (empty($this->ignore) and ($force === true)) {
+            $this->ignore = new Iterator();
+        }
+
+        return $this->ignore;
+    }
+
+
+    /**
+     * Sets the ignore list
+     *
+     * @param IteratorInterface|null $ignore
+     *
+     * @return static
+     */
+    public function setIgnoreObject(?IteratorInterface $ignore): static
+    {
+        $this->ignore = $ignore;
+        return $this;
+    }
+}
---
+++ Phoundation/Data/Traits/TraitDataSkipIterator.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Trait TraitDataSkipIterator
+ *
+ * This trait contains the basic methods required to use a skip iterator list
+ *
+ * The skip list can be used to track a list of items that should be skipped
+ *
+ * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license   http://openskip.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package   Phoundation\Data
+ */
+
+
+declare(strict_types=1);
+
+namespace Phoundation\Data\Traits;
+
+use Phoundation\Data\Interfaces\IteratorInterface;
+use Phoundation\Data\Iterator;
+
+trait TraitDataSkipIterator
+{
+    /**
+     * The list of items to skip
+     *
+     * @var IteratorInterface|null $skip
+     */
+    protected ?IteratorInterface $skip = null;
+
+
+    /**
+     * Returns the skip list
+     *
+     * @param bool $force
+     *
+     * @return IteratorInterface|null
+     */
+    public function getSkipObject(bool $force = false): ?IteratorInterface
+    {
+        if (empty($this->skip) and ($force === true)) {
+            $this->skip = new Iterator();
+        }
+
+        return $this->skip;
+    }
+
+
+    /**
+     * Sets the skip list
+     *
+     * @param IteratorInterface|null $skip
+     *
+     * @return static
+     */
+    public function setSkipObject(?IteratorInterface $skip): static
+    {
+        $this->skip = $skip;
+        return $this;
+    }
+}
