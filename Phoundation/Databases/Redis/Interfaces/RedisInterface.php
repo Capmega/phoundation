a0cc98d113e9585382e7638bd269e1a24499f397
Harrison Macey <harrison@medinet.ca>
2025-03-05 16:41:47 -0800

    Improved Databases library Fixed bug in Redis::get() parameter types
    Signed-off-by: Harrison Macey <harrison@medinet.ca>

--- Phoundation/Databases/Redis/Interfaces/RedisInterface.php
+++ Phoundation/Databases/Redis/Interfaces/RedisInterface.php
@@ -16,11 +16,13 @@ interface RedisInterface extends DatabaseInterface
     /**
      * Returns a value for the specified key
      *
-     * @param string $key
+     *
+     * @param string|float|int|null $key
+     * @param callable|null         $cache_callback *
      *
      * @return mixed
      */
-    public function get(string $key): mixed;
+    public function get(string|float|int|null $key, ?callable $cache_callback): mixed;
 
 
     /**
