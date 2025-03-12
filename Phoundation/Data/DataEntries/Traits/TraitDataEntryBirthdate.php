a4718e8220b474f54f79f17941be4ec30662a015
Harrison Macey <harrison@medinet.ca>
2025-03-12 15:15:41 -0700

    Improved Data library
    Fixed bugs in TraitDataEntryBirthdate
    
    Signed-off-by: Harrison Macey <harrison@medinet.ca>

--- Phoundation/Data/DataEntries/Traits/TraitDataEntryBirthdate.php
+++ Phoundation/Data/DataEntries/Traits/TraitDataEntryBirthdate.php
@@ -19,33 +19,76 @@ namespace Phoundation\Data\DataEntries\Traits;
 
 
 use Phoundation\Date\PhoDateTime;
+use Phoundation\Exception\OutOfBoundsException;
 
 trait TraitDataEntryBirthdate
 {
     /**
      * Returns the birthdate day for this object
      *
-     * @return PhoDateTime|string|null
+     * @return string|null
      */
-    public function getBirthdate(): PhoDateTime|string|null
+    public function getBirthdate(): string|null
     {
-        return PhoDateTime::new($this->getTypesafe('string', 'birthdate'));
+        return $this->getTypesafe('string', 'birthdate');
     }
 
 
     /**
      * Sets the birthdate day for this object
      *
-     * @param PhoDateTime|string|null $birthdate
+     * @param PhoDateTime|array|string|null $birthdate
      *
      * @return static
      */
-    public function setBirthdate(PhoDateTime|string|null $birthdate): static
+    public function setBirthdate(PhoDateTime|array|string|null $birthdate): static
     {
+        if (is_array($birthdate)) {
+            try {
+                $birthdate = array_get('date', $birthdate);
+
+            } catch (\Throwable $e) {
+                throw OutOfBoundsException::new(tr('Unknown array given, unable to set birthdate'))
+                                          ->addData([
+                                              'array' => $birthdate,
+                                              'exception' => $e
+                                          ]);
+            }
+        }
+
         if ($birthdate instanceof PhoDateTime) {
             $birthdate->format('Y-m-d');
         }
 
+        return $this->set(get_null($birthdate), 'birthdate');
+    }
+
+
+    /**
+     * Returns the Birthdate as a PhoDateTime Object
+     *
+     * @return PhoDateTime|null
+     */
+    public function getBirthdateObject(): PhoDateTime|null
+    {
+        $birthdate = $this->getBirthdate();
+        if ($birthdate) {
+            return PhoDateTime::new($birthdate);
+        }
+
+        return null;
+    }
+
+
+    /**
+     * Sets the birthdate by date object
+     *
+     * @param PhoDateTime|null $birthdate
+     *
+     * @return static
+     */
+    public function setBirthdateObject(PhoDateTime|null $birthdate): static
+    {
         return $this->set(get_null($birthdate), 'birthdate');
     }
 }
