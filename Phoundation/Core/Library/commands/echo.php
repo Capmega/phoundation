bd0dc81ec4e35f6d73f81cc74bfb00d60ad4b17f
Sven Olaf Oostenbrink <sven@medinet.ca>
2025-04-15 20:09:09 -0700

    Fixed and improved Core library
    Added new core command "echo"
    
    Fixed documentation
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

---
+++ Phoundation/Core/Library/commands/echo.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Command "echo"
+ *
+ * The echo script will echo the specified text. This command is mostly used for debugging purposes.
+ *
+ * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright © 2022 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package   Phoundation\Core
+ */
+
+
+declare(strict_types=1);
+
+use Phoundation\Cli\CliDocumentation;
+use Phoundation\Core\Log\Log;
+use Phoundation\Data\Validator\ArgvValidator;
+use Phoundation\Filesystem\PhoDirectory;
+use Phoundation\Os\Processes\Commands\Find;
+use Phoundation\Utils\Strings;
+
+
+CliDocumentation::setUsage('./pho echo TEXT');
+
+CliDocumentation::setHelp('The echo script will echo the specified text. This command is mostly used for debugging 
+purposes.
+
+
+ARGUMENTS
+
+
+-');
+
+
+// Get arguments
+$argv = ArgvValidator::new()
+                     ->select('text')->isString()
+                     ->validate();
+
+
+Log::cli($argv['text']);
