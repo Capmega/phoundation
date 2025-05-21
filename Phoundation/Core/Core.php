349ed1372efb81d96813f428b2a2abb05e0ca8b6
Sven Olaf Oostenbrink <sven@medinet.ca>
2025-05-21 14:45:55 -0700

    Bumped core version to 4.15
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Core/Core.php
+++ Phoundation/Core/Core.php
@@ -86,7 +86,7 @@ class Core implements CoreInterface
     /**
      * Framework version and minimum required PHP version
      */
-    public const string PHOUNDATION_VERSION = '4.14.0';
+    public const string PHOUNDATION_VERSION = '4.15.0';
 
     public const string PHP_MINIMUM_VERSION = '8.3.0';
 
