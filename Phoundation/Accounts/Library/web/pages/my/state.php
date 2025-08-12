e5593d9b595847c109dab1c693647499af33f501
Sven Olaf Oostenbrink <sven@medinet.ca>
2025-08-12 13:26:40 -0700

    Improved Accounts library
    WIP implementing State class that will track page state across all pages within the same session
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

---
+++ Phoundation/Accounts/Library/web/pages/my/state.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Page my/state.php
+ *
+ *
+ *
+ * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright © 2025 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package   Phoundation\Accounts
+ */
+
+
+declare(strict_types=1);
+
+use Phoundation\Accounts\Users\Sessions\Session;
+use Phoundation\Data\Validator\GetValidator;
+use Phoundation\Web\Html\Components\Anchor;
+use Phoundation\Web\Requests\Response;
+
+
+// No get parameters allowed
+GetValidator::new()->validate();
+
+
+// Set page meta data
+Response::setHeaderTitle(tr('My session sate'));
+Response::setHeaderSubTitle(Session::getUserObject()->getDisplayName());
+Response::setBreadcrumbs([
+    Anchor::new('/'       , tr('Home')),
+    Anchor::new('/my.html', tr('My pages')),
+    Anchor::new(''        , tr('My state information')),
+]);
+
+
+
+show(Session::getStateObject()->getSource());
