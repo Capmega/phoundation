d250823a2339f15a2df391d9d20c14c8be80e5f7
Sven Olaf Oostenbrink <sven@medinet.ca>
2026-01-05 09:57:23 -0800

    Fixed Developer library
    Fixed command dev git diff, now allows directory paths as well
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Developer/Library/commands/developer/git/diff.php
+++ Phoundation/Developer/Library/commands/developer/git/diff.php
@@ -22,7 +22,7 @@ use Phoundation\Developer\Versioning\Git\Git;
 use Phoundation\Filesystem\PhoDirectory;
 
 
-CliDocumentation::setUsage('./pho development git diff
+CliDocumentation::setUsage('./pho development git diff PATHS
 ./pho dev git diff');
 
 CliDocumentation::setHelp('This command is an interface to the git command through Phoundation. Its not really needed -nor
@@ -31,7 +31,7 @@ useful- beyond testing the git library');
 
 // Get command line arguments
 $argv = ArgvValidator::new()
-                     ->select('files')->sanitizeForceArray()->forEachField()->sanitizeFile()
+                     ->select('files')->sanitizeForceArray()->forEachField()->sanitizePath()
                      ->validate();
 
 
