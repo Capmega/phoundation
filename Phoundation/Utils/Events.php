58019154660b768210e7ac3f5b698dc277ec32b4
Sven Olaf Oostenbrink <sven@medinet.ca>
2026-01-13 13:10:49 -0800

    Fixed Events library
    Fixed interface issues
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Utils/Events.php
+++ Phoundation/Utils/Events.php
@@ -52,12 +52,12 @@ class Events extends IteratorCore implements EventsInterface
     /**
      * Triggers the callback for the specified events
      *
-     * @param Stringable|string|float|int $event     The event key for which the callback should be executed
-     * @param mixed                       $values    The values to pass along to the callback function
-     * @param bool                        $exception If true, will throw a NotExistsException if the specified event
-     *                                               does not exist
-     * @return mixed                                 The return value from the event callback, if available. NULL
-     *                                               otherwise
+     * @param Stringable|string|float|int $event                          The event key for which the callback should be executed
+     * @param mixed                       $values                         The values to pass along to the callback function
+     * @param bool                        $delete_after_execution [false] If true, this Events object will delete this event once it has been executed
+     * @param bool                        $exception              [false] If true, will throw a NotExistsException if the specified event does not exist
+     *
+     * @return mixed                                                      The return value from the event callback, if available. NULL otherwise
      */
     public function trigger(Stringable|string|float|int $event, mixed $values, bool $delete_after_execution = false, bool $exception = false): mixed
     {
@@ -93,7 +93,7 @@ class Events extends IteratorCore implements EventsInterface
     /**
      * @inheritDoc
      */
-    #[ReturnTypeWillChange] public function get(Stringable|string|float|int $key, mixed $default = null, ?bool $exception = null): callable
+    #[ReturnTypeWillChange] public function get(Stringable|string|float|int $key, mixed $default = null, ?bool $exception = null): ?callable
     {
         return parent::get($key, $default, $exception);
     }
