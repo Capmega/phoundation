25a36ba2c022abf4ee1e00babb75ffb8b646731d
Sven Olaf Oostenbrink <sven@medinet.ca>
2026-01-22 15:05:17 -0800

    Improved Filesystem, Content libraries
    Merged methods from Filesystem\Filetypes\FsPdf into Content\Documents\Pdf class
    
    Removed class path Filesystem\Filetypes
    
    Added Pdf::isPasswordProtected(), Pdf::addPassword() (WIP), Pdf::removePassword() (WIP)
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Content/Documents/Pdf.php
+++ Phoundation/Content/Documents/Pdf.php
@@ -17,13 +17,123 @@ declare(strict_types=1);
 
 namespace Phoundation\Content\Documents;
 
+use Phoundation\Content\Documents\Exception\PdfPasswordProtectedException;
+use Phoundation\Data\Entry;
+use Phoundation\Data\Interfaces\EntryInterface;
+use Phoundation\Data\Traits\TraitDataPassword;
 use Phoundation\Filesystem\PhoDirectory;
 use Phoundation\Filesystem\PhoFile;
 use Phoundation\Filesystem\PhoFiles;
-
+use Phoundation\Os\Processes\Exception\ProcessFailedException;
+use Phoundation\Os\Processes\Process;
+use Phoundation\Utils\Strings;
 
 class Pdf extends PhoFile
 {
+    use TraitDataPassword;
+
+
+    /**
+     * This method will return
+     *
+     * @return int
+     */
+    public function getPageCount(): int
+    {
+        return (int) $this->getInfo()->get('pages');
+    }
+
+
+    /**
+     * Makes this PDF file password protected
+     *
+     * @param string $password
+     *
+     * @return static
+     */
+    public function addPassword(string $password): static
+    {
+        //qpdf --password=yourpassword --decrypt input.pdf output.pdf
+        return $this;
+    }
+
+
+    /**
+     * Removes the password from this PDF file
+     *
+     * @param string $password
+     *
+     * @return static
+     */
+    public function removePassword(string $password): static
+    {
+        //qpdf --password=yourpassword --decrypt input.pdf output.pdf
+        return $this;
+    }
+
+
+    /**
+     * Returns true if this PDF file is password protected
+     *
+     * @return bool
+     */
+    public function isPasswordProtected(): bool
+    {
+        try {
+            $this->getInfo();
+
+        } catch (PdfPasswordProtectedException) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Returns an Entry object with basic information about this PDF document
+     *
+     * @return EntryInterface
+     *
+     * @throws ProcessFailedException
+     * @throws PdfPasswordProtectedException
+     */
+    public function getInfo(): EntryInterface
+    {
+        $this->checkRestrictions(false);
+
+        try {
+            $data    = [];
+            $results = Process::new()
+                              ->setCommand('pdfinfo')
+                              ->addArguments($this->source)
+                              ->executeReturnArray();
+
+        } catch (ProcessFailedException $e) {
+            if ($e->dataContains('incorrect password')) {
+                throw new PdfPasswordProtectedException(ts('Cannot process PDF file ":file", the file is password protected and no password was provided', [
+                    ':file' => $this->source
+                ]));
+            }
+
+            throw $e;
+        }
+
+        // Parse the command output
+        foreach ($results as $line) {
+            $data = array_replace($data, Strings::split($line, ':'));
+        }
+
+        // Fix data
+        if (array_key_exists('file size', $data)) {
+            $data['size'] = (int) $data['file size'];
+            unset($data['file size']);
+        }
+
+        return Entry::new()->setSource($data);
+    }
+
+
     /**
      * Will split this PDF file by pages
      *
---
+++ Phoundation/Filesystem/Exception/Interfaces/FilePasswordProtectedExceptionInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Interface FilePasswordProtectedExceptionInterface
+ *
+ * This interface should be used for any and all "file is password protected" exceptions
+ *
+ * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright © 2025 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package   Phoundation\Filesystem
+ */
+
+
+declare(strict_types=1);
+
+namespace Phoundation\Filesystem\Exception\Interfaces;
+
+
+interface FilePasswordProtectedExceptionInterface
+{
+}
