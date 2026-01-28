370bc1ea3a29c7ebf816a84fd3bcd13aab201430
Sven Olaf Oostenbrink <sven@medinet.ca>
2026-01-28 09:25:12 -0800

    Improved Utils library
    Improved Strings::cut() added $instance support
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Utils/Strings.php
+++ Phoundation/Utils/Strings.php
@@ -1133,9 +1133,9 @@ class Strings extends Utils
      * /code
      *
      */
-    public static function cut(Stringable|string|int|null $source, Stringable|string|int $start, Stringable|string|int $stop, bool $needles_required = true, bool $case_insensitive = false): string
+    public static function cut(Stringable|string|int|null $source, Stringable|string|int $start, Stringable|string|int $stop, int $instance = 1, bool $needles_required = true, bool $case_insensitive = false): string
     {
-        return Strings::until(Strings::from($source, $start, needle_required: $needles_required, case_insensitive: $case_insensitive), $stop, needle_required: $needles_required, case_insensitive: $case_insensitive);
+        return Strings::until(Strings::from($source, $start, $instance, needle_required: $needles_required, case_insensitive: $case_insensitive), $stop, needle_required: $needles_required, case_insensitive: $case_insensitive);
     }
 
 
