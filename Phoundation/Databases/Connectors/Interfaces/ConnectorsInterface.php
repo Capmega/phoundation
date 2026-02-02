fe67e72fd150482229d9829fb642cfebf5af97c9
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2026-02-01 16:58:00 -0800

    Improved Databases library
    Improved Connectors interface
    
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

--- Phoundation/Databases/Connectors/Interfaces/ConnectorsInterface.php
+++ Phoundation/Databases/Connectors/Interfaces/ConnectorsInterface.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Phoundation\Databases\Connectors\Interfaces;
 
+use Phoundation\Data\DataEntries\Interfaces\DataEntryInterface;
 use Phoundation\Data\DataEntries\Interfaces\DataIteratorInterface;
+use Phoundation\Databases\Connectors\Connector;
+use Phoundation\Exception\OutOfBoundsException;
+use ReturnTypeWillChange;
 use Stringable;
 
 interface ConnectorsInterface extends DataIteratorInterface
@@ -20,4 +24,34 @@ interface ConnectorsInterface extends DataIteratorInterface
      * ConnectorInterface
      */
     public function getConnectorWithDatabase(string|int $connector, string $database): ConnectorInterface;
+
+    /**
+     * Returns only the specified key from the source of this DataEntry
+     *
+     * @note This method filters out all keys defined in static::getProtectedKeys() to ensure that keys like "password"
+     *       will not become available outside this object
+     *
+     * @param Stringable|string|float|int $key
+     * @param mixed                       $default
+     * @param bool                        $exception
+     *
+     * @return ConnectorInterface|null
+     */
+    #[ReturnTypeWillChange] public function get(Stringable|string|float|int $key, mixed $default = null, ?bool $exception = null): ?ConnectorInterface;
+
+    /**
+     * Returns a random connector
+     *
+     * @return DataEntryInterface|null
+     */
+    #[ReturnTypeWillChange] public function getRandom(): ?DataEntryInterface;
+
+    /**
+     * Returns the current entry
+     *
+     * @note overrides the IteratorCore::current() method which returns mixed
+     *
+     * @return DataEntryInterface|null
+     */
+    #[ReturnTypeWillChange] public function current(): ?DataEntryInterface;
 }
