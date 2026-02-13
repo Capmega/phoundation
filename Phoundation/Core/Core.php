2448ef79729749146117bbb3e8fe84dbea8ae2c9
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2026-02-12 17:23:43 -0800

    Updated Core library to v4.19
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

--- Phoundation/Core/Core.php
+++ Phoundation/Core/Core.php
@@ -102,7 +102,7 @@ class Core implements CoreInterface
     /**
      * Framework version and minimum required PHP version
      */
-    public const string PHOUNDATION_VERSION = '4.18.0';
+    public const string PHOUNDATION_VERSION = '4.19.0';
 
     public const string PHP_MINIMUM_VERSION = '8.3.0';
 
