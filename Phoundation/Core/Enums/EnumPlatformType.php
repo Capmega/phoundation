f213fee5c20d56389bf0d7fbeaee9a1f4cb8a039
Sven Olaf Oostenbrink <sven@medinet.ca>
2026-02-27 10:15:51 -0800

    Improved Core library
    Added EnumPlatformType
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

---
+++ Phoundation/Core/Enums/EnumPlatformType.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Enum EnumPlatformType
+ *
+ * This enum contains the two different platform types for Phoundation: web and cli
+ *
+ * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright © 2025 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package   Phoundation\Core
+ */
+
+
+declare(strict_types=1);
+
+namespace Phoundation\Core\Enums;
+
+
+enum EnumPlatformType: string
+{
+    case web   = 'web';
+    case cli   = 'cli';
+}
