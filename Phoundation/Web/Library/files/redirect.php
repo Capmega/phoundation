3d5450c84ed34b45d1bfad3c804438b247e0a8e5
Sven Olaf Oostenbrink <sven@medinet.ca>
2026-03-20 10:52:49 -0700

    (WIP) Improving Web library
    Implementing Redirect classes to automatically manage web page redirects
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

---
+++ Phoundation/Web/Library/files/redirect.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Page :PAGE
+ *
+ * This is a MOVED PERMANENTLY redirection page that will redirect the current location to the new location
+ *
+ * This page is generated automatically by the command "web move"
+ *
+ * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @copyright Copyright © 2025 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @package   Phoundation\Web
+ */
+
+
+declare(strict_types=1);
+
+use Phoundation\Web\Requests\Response;
+
+
+// Get request data
+Response::redirect(':URL');
---
+++ Phoundation/Web/Requests/Redirects/Redirect.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Class Redirect
+ *
+ * This class manages a single web redirect
+ *
+ * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @copyright Copyright © 2025 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @package   Phoundation\Web
+ */
+
+
+declare(strict_types=1);
+
+namespace Phoundation\Web\Requests\Redirects;
+
+use Phoundation\Data\DataEntries\DataEntry;
+
+class Redirect extends DataEntry
+{
+
+}
